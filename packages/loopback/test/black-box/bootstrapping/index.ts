d406a0df687c2828e4f1863358837cefd0e23be8
Ritchie Martori <skawful@gmail.com>
2017-01-24 14:38:34 -0800

    Add global scenarios test utility

--- packages/loopback/test/black-box/bootstrapping/index.ts
+++ packages/loopback/test/black-box/bootstrapping/index.ts
@@ -1,5 +1,7 @@
-describe('User starting app', () => {
+scenarios('User starting app', (container, expect) => {
   context('Given an app using default configs', () => {
-    it('will start the app on port 3000');
+    it('will start the app on port 3000', () => {
+      expect(true).to.be.true();
+    });
   });
 });
--- packages/loopback/test/support/scenarios.ts
+++ packages/loopback/test/support/scenarios.ts
@@ -1,8 +1,8 @@
 import container from './container';
 import expect from './expect';
 
-global.scenarios = function(description : string, cb) {
+global['scenarios'] = function(description : string, cb) {
   describe(description, () => {
     cb(container, expect);
   });
-}
+};
