92dd73436d938d544b1012898b1cde15b7ec92ab
Miroslav Bajtoš <mbajtos@cz.ibm.com>
2017-02-08 13:24:43 +0100

    Add TSLint to the workflow
    Configure TSLint checking for common problems.
    
    There is only a single task to lint all source files in one go.
    This is due to the limitation of tslint formatter which is not able
    to always produce absolute paths, and the limitation of VSCode+lerna
    which cannot detect the individual package directory where a
    per-package linter would be run via `npm run lint`. As a result, a path
    relative to package root (e.g. "packages/loopback") cannot be converted
    to an absolute path to offer in the Problems window.
    
    Usage options:
    
     - `npm run lint` at repo root
     - `npm run test` runs linter after the tests too
     - Run "Lint all files" task in VS code. Check the Problems window
       for the list of issues to fix
     - Install "vscode-tslint" to your VisualStudio Code to get real-time
       checking of a subset of linting rules
    
    As part of this patch, I fixed missing type declarations in
    "bootstrapping.ts" test and also fixed "client.ts" to resolve the
    returned promise to a value which honours the declared interface
    ("status" vs. "statusCode" property).

--- packages/loopback/test/support/client.ts
+++ packages/loopback/test/support/client.ts
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2013,2017. All Rights Reserved.
+// Node module: loopback
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 import { Application, AppConfig } from 'loopback';
 import bluebird = require('bluebird');
 import request = require('request-promise');
@@ -7,22 +12,22 @@ export class Client {
 
   }
 
-  public get(path : string) : Promise<Result>{
+  public async get(path : string) : Promise<Client.Result> {
     const url = 'http://localhost:' + this.app.config.port + path;
-    let options = {
+    const options = {
       uri: url,
-      resolveWithFullResponse: true
+      resolveWithFullResponse: true,
     } as any;
 
-    return request(options)
-      .then((response) => {
-        return {
-          statusCode: response.statusCode
-        };
-      });
+    const response = await request(options);
+    return {
+      status: response.statusCode,
+    };
   }
 }
 
-interface Result {
-  status : number;
+export module Client {
+  export interface Result {
+    status : number;
+  }
 }
