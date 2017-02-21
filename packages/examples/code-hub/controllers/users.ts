bce08113f9ee079e790bc992e9f7112c3cd94dfc
Ritchie Martori <skawful@gmail.com>
2017-02-21 12:05:30 -0800

    Cleanup

--- packages/examples/code-hub/controllers/users.ts
+++ packages/examples/code-hub/controllers/users.ts
@@ -2,20 +2,32 @@ import spec = require('./users.api');
 import {Controller, api, inject} from "loopback";
 
 @api(spec)
-class UserController extends Controller {
+export class UserController extends Controller {
   public getUserByUsername(username : string) : UserResponse {
 
   }
 
-  public getAuthenticatedUser(@inject('userId') userId : string) : UserResponse {
-
+  public getAuthenticatedUser(@inject('userId') userId : number) : UserResponse {
+    if(userId === 42) {
+      let ur = UserResponse();
+      ur.id = userId;
+      ur.name = 'Foo Bar';
+      return ur;
+    } else {
+      throw new NotFoundError();
+    }
   }
 
-  public updateAuthenticatedUser(@inject('userId') userId : string) : UserRespone {
+  public updateAuthenticatedUser(@inject('userId') userId : number) : UserRespone {
 
   }
 
   public getAllUsers(since : string) : Array<UserResponse> {
 
   }
+}
+
+
+class NotFoundError {
+  code : number = 404;
 }
