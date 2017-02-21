24cee8d12e9186d6008519639f8837dd08a31e20
Ritchie Martori <skawful@gmail.com>
2017-02-21 12:05:30 -0800

    Cleanup

--- packages/examples/code-hub/index.ts
+++ packages/examples/code-hub/index.ts
@@ -1,7 +1,15 @@
-import {Application, Server} from "loopback";
+import {Application, Server, AccessToken} from "loopback";
+import {UserController} from "./controllers/users";
 
 let app = new Application();
 let server = new Server();
 
+server.bind('applications.app').to(app);
+app.bind('controllers.users').to(UserController);
+app.bind('userId').to(42);
+
+await server.start();
+
+console.log('server listenting on', server.info());
 
 
