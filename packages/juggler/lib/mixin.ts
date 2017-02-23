ef65effb74968aa62695356f81d7398d711591ca
Raymond Feng <rfeng@us.ibm.com>
2017-02-23 13:55:31 -0800

    Enhance mixins

--- packages/juggler/lib/mixin.ts
+++ packages/juggler/lib/mixin.ts
@@ -1,24 +1,34 @@
 /**
  * A constructor
  */
-export type Constructable = new (...args: any[]) => Object;
+export type Constructor = new (...args: any[]) => Object;
 
 /**
  * Interface for functions that can mix properties/methods into a base class
  */
 export interface MixinFunc {
-  <BC extends Constructable>(Base: BC): BC;
+  <BC extends Constructor>(Base: BC): BC;
 }
 
 export class MixinBuilder {
-  constructor(public superclass) {
+  constructor(public baseClass) {
   }
 
   with(...mixins: MixinFunc[]) {
-    return mixins.reduce((c, mixin) => mixin(c), this.superclass);
+    return mixins.reduce((c, mixin) => mixin(c), this.baseClass);
   }
 
-  static mix(superClass) {
-    return new MixinBuilder(superClass);
+  static mix(baseClass) {
+    return new MixinBuilder(baseClass);
   }
+}
+
+
+function extend(superClass: Constructor, ...mixins: Constructor[]): Constructor {
+  const mixed = class extends superClass {
+  };
+  Object.assign(mixed, ...mixins);
+  let prototypes: Constructor[] = mixins.map((c) => c.prototype);
+  Object.assign(mixed.prototype, ...prototypes);
+  return mixed;
 }
