be05229c9fe12cba09a4cb9323afae41aa145647
Raymond Feng <rfeng@us.ibm.com>
2017-03-01 14:44:52 -0800

    Add more comments

--- packages/juggler/lib/dao.ts
+++ packages/juggler/lib/dao.ts
@@ -1,9 +1,16 @@
 import {DataSource, ModelBaseClass as Model, KeyValueAccessObject} from 'loopback-datasource-juggler';
 import DAO = require('loopback-datasource-juggler/lib/dao');
 
-import {MixinBuilder, Constructable} from './mixin';
+import {MixinBuilder, Constructor} from './mixin';
 
-module.exports = function (ds: DataSource, Model: Constructable) {
+/**
+ * This is a bridge to the legacy DAO class. The function mixes DAO methods
+ * into a model class and attach it to a given data source
+ * @param ds {DataSource} Data source
+ * @param Model {} Model class
+ * @returns {} The new model class with DAO (CRUD) operations
+ */
+module.exports = function (ds: DataSource, Model: Constructor) {
   let BoundModel = class extends Model {
     static dataSource: DataSource;
   };
