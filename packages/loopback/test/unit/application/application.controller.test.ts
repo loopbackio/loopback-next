633292f5a1fbc7c8a9f3e8675fc08b3da9a432ea
Miroslav Bajtoš <mbajtos@cz.ibm.com>
2017-03-02 16:16:37 +0100

    Add "app.controller()" sugar API

---
+++ packages/loopback/test/unit/application/application.controller.test.ts
@@ -0,0 +1,21 @@
+// Copyright IBM Corp. 2013,2017. All Rights Reserved.
+// Node module: loopback
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {expect} from 'testlab';
+import {Application} from './../../../lib/application';
+
+describe('Application.controller()', () => {
+  it('binds the controller to "controllers.*" namespace', () => {
+    const app = new Application();
+
+    class TestController {
+    }
+
+    app.controller(TestController);
+
+    const boundControllers = app.find('controllers.*').map(b => b.key);
+    expect(boundControllers).to.contain('controllers.TestController');
+  });
+});
