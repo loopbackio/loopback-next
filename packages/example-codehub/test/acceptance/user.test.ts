3ed9ee1d386880062b86821c7093f67fcce88a0f
Miroslav Bajtoš <mbajtos@cz.ibm.com>
2017-04-05 11:04:52 +0200

    codehub: add acceptance tests for User controller

---
+++ packages/example-codehub/test/acceptance/user.test.ts
@@ -0,0 +1,38 @@
+// Copyright IBM Corp. 2013,2017. All Rights Reserved.
+// Node module: loopback
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import { CodeHubApplication } from 'example-codehub/src/CodeHubApplication';
+import {expect, supertest as request} from 'testlab';
+import * as util from 'example-codehub/test/support/util';
+
+describe('users', () => {
+  let app: CodeHubApplication;
+  let client: request.SuperTest<request.Test>;
+
+  before(givenClientAndApp);
+
+  it('gets all users', async () => {
+    const response = await client.get('/users')
+      .expect('Content-Type', /json/)
+      .expect(200);
+    expect(response.body).to.eql([]);
+  });
+
+  it('gets user by name', async () => {
+    const response = await client.get('/users/admin')
+      .expect('Content-Type', /json/)
+      .expect(200);
+    expect(response.body).to.eql({
+      name: 'admin',
+    });
+  });
+
+  async function givenClientAndApp() {
+    const result = await util.createAppAndClient();
+    client = result.client;
+    app = result.app;
+  }
+});
+
