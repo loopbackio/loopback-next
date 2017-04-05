2578dbc353b8bee80ed196976cf17d5f156a4e59
Raymond Feng <rfeng@us.ibm.com>
2017-04-05 15:56:06 -0700

    Add more samples

--- packages/juggler/lib/connector.ts
+++ packages/juggler/lib/connector.ts
@@ -1,10 +1,13 @@
+import {Model} from './model';
+
 /**
  * Minimum contract for connectors
  */
 export interface Connector {
-  name: string;
-  interfaces: string[];
-  connect(): Promise<any>;
-  disconnect(): Promise<any>;
-  ping(): Promise<any>;
+  name: string; // Name/type of the connector
+  configModel: Model; // The configuration model
+  interfaces: string[]; // A list of interfaces implemented by the connector
+  connect(): Promise<any>; // Connect to the underlying system
+  disconnect(): Promise<any>; // Disconnect from the underlying system
+  ping(): Promise<any>; // Ping the underlying system
 }
