72ac566b922f3da785a462e42a382daa4d4daca1
Miroslav Bajtoš <mbajtos@cz.ibm.com>
2017-04-05 14:19:00 +0200

    Migrate from chai.js to should.js

--- packages/loopback/test/acceptance/context-bindings/finding-bindings.ts
+++ packages/loopback/test/acceptance/context-bindings/finding-bindings.ts
@@ -22,7 +22,7 @@ describe('Context bindings - Finding bindings', () => {
         const keys = bindings.map(binding => {
           return binding.key;
         });
-        expect(keys).to.include('foo', 'baz');
+        expect(keys).to.containDeep(['foo', 'baz']);
       });
     });
 
@@ -40,8 +40,8 @@ describe('Context bindings - Finding bindings', () => {
       it('returns all bindings matching the pattern', () => {
         const bindings = ctx.find('my.*');
         const keys = bindings.map(binding => binding.key);
-        expect(keys).to.include('my.foo', 'my.baz');
-        expect(keys).to.not.include('ur.quux');
+        expect(keys).to.containDeep(['my.foo', 'my.baz']);
+        expect(keys).to.not.containDeep(['ur.quux']);
       });
     });
   });
@@ -54,7 +54,7 @@ describe('Context bindings - Finding bindings', () => {
       it('returns all bindings matching the tag', () => {
         const bindings = ctx.findByTag('dog');
         const dogs = bindings.map(binding => binding.key);
-        expect(dogs).to.include('spot', 'fido');
+        expect(dogs).to.containDeep(['spot', 'fido']);
       });
     });
 
