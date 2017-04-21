7898906884376546fd8186bc19253ec099c9d959
deepakrkris <deepak.r.kris@gmail.com>
2017-04-21 12:18:16 -0700

    POC to mock passport and express
    * use minimal passport to check dependencies
    
    * use minimal express to check dependencies

---
+++ packages/authentication/test/poc/mock-passport.js
@@ -0,0 +1,55 @@
+var http = require('http');
+
+class ExpressAdapter {
+  constructor() { 
+    this.authentication = new Authentication();
+  }
+
+  initialize() {
+    return function(req, res, next) {
+      next();
+    }
+  }
+
+  use(strategy) {
+    this.authentication.setStrategy(strategy);
+  }
+
+  authenticate() {
+    return this.authentication.authenticate.bind(this.authentication);
+  }
+};
+
+export class Authentication {
+  constructor() {}
+
+  setStrategy(strategy) {
+    this.strategyCtor = strategy;
+  }
+
+  authenticate(req, res, next) {
+    var strategy = Object.create(this.strategyCtor);
+    var self = this;
+    req.logIn = req.login = function(user, done) {
+      this.user = user;
+      done();
+    }
+    strategy.success = function(user, info) {
+      req.login(user, next);
+    }
+    strategy.fail = function(challenge, status) {
+      self.reAuthenticate(res, challenge, status);
+    }
+    strategy.authenticate(req);
+  }
+
+  reAuthenticate(res, challenge, status) {
+    res.statusCode = status || 401;
+    if (res.statusCode == 401 && challenge) {
+      res.setHeader('WWW-Authenticate', [challenge]);
+    }
+    res.end(http.STATUS_CODES[res.statusCode]);
+  }
+}
+
+module.exports = new ExpressAdapter();
