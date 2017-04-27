f1f83e9fac8f5091a4bd795eae71a9fbc6eba2c9
Ritchie Martori <skawful@gmail.com>
2017-04-27 12:23:15 -0700

    implement authenticate annotation
    * Create a new metadata file
    * Create a new unit test case file

---
+++ packages/authentication/src/decorator.ts
@@ -0,0 +1,53 @@
+// Copyright IBM Corp. 2017. All Rights Reserved.
+// Node module: @loopback/authenticate
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import 'reflect-metadata';
+
+/* interface to define authentication metadata structure expected in
+ * json objects
+ */
+export interface IMetadata {
+  // name of the passport strategy to use for authentication
+  strategy: string;
+  // options to configure the passport strategy
+  options: Object;
+}
+
+/* class to hold authentication metadata
+ */
+export class AuthenticationMetadata {
+  strategyName: string;
+  options: Object;
+
+  constructor(private strategy: string, private optionValues?: Object) {
+    this.strategyName = strategy;
+    this.options = optionValues || {};
+  }
+  getMetadata(): IMetadata {
+    return {strategy: this.strategyName, options: this.options};
+  }
+}
+
+/**
+ * decorator to add authentication metadata to controller methods
+ * @param strategyName
+ * @param options
+ */
+export function authenticate(strategyName: string, options? : Object) {
+  return function(controllerClass: Object, methodName: string) {
+    const metadataObj: AuthenticationMetadata = new AuthenticationMetadata(strategyName, options || {});
+    Reflect.defineMetadata('loopback:authenticate', metadataObj, controllerClass, methodName);
+  };
+}
+
+/**
+ * function to get stored authentication metadata in a controller method
+ * @param controllerObj
+ * @param methodName
+ */
+export function getAuthenticateMetadata(controllerObj: Object, methodName: string): IMetadata {
+  const metadataObj: AuthenticationMetadata = Reflect.getMetadata('loopback:authenticate', controllerObj, methodName);
+  return metadataObj.getMetadata();
+}
--- packages/authentication/test/unit/authenticate.test.ts
+++ packages/authentication/test/unit/authenticate.test.ts
@@ -1,24 +1,53 @@
-// Copyright IBM Corp. 2013,2017. All Rights Reserved.
+// Copyright IBM Corp. 2017. All Rights Reserved.
 // Node module: loopback
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
 import {expect} from 'testlab';
-import {authenticate, getAuthenticateMetadata} from '../../src/authenticate';
+import {authenticate, getAuthenticateMetadata, AuthenticationMetadata} from '../../src/decorator';
 
-describe('@authenticate decorator', () => {
-  it('can add authenticate metadata to target method', () => {
-    class TestClass {
-     constructor() {}
+describe('Authentication', () => {
+  describe('Class AuthenticationMetadata type', () => {
+    it('can create authentication descriptor for a strategy', () => {
+      const metadataObj: AuthenticationMetadata = new AuthenticationMetadata('my-strategy', {option: 'my-options'});
+      const metaData = metadataObj.getMetadata();
+      const expectedMetaDataObject = {strategy: 'my-strategy', options: {option: 'my-options'}};
+      expect(metaData).to.deepEqual(expectedMetaDataObject);
+    });
 
-     @authenticate
-     async whoAmI() {
-      return;
-     }
-   }
+    it('can create authentication descriptor with strategy and options', () => {
+      const metadataObj: AuthenticationMetadata = new AuthenticationMetadata('my-strategy', {option: 'my-options'});
+      const metaData = metadataObj.getMetadata();
+      const expectedMetaDataObject = {strategy: 'my-strategy', options: {option: 'my-options'}};
+      expect(metaData).to.deepEqual(expectedMetaDataObject);
+    });
+  });
 
-    const test: TestClass = new TestClass();
-    const metaData = getAuthenticateMetadata(test, 'whoAmI');
-    expect(metaData).to.deepEqual(true);
+  describe('@authenticate decorator', () => {
+    it('can add authenticate metadata to target method', () => {
+      class TestClass {
+        @authenticate('my-strategy', {option1: 'value1', option2: 'value2'})
+        async whoAmI() {}
+      }
+
+      const test: TestClass = new TestClass();
+
+      const metaData = getAuthenticateMetadata(test, 'whoAmI');
+      const expectedMetaDataObject = {strategy: 'my-strategy', options: {option1: 'value1', option2: 'value2'}};
+      expect(metaData).to.deepEqual(expectedMetaDataObject);
+    });
+
+    it('can add authenticate metadata to target method without options', () => {
+      class TestClass {
+        @authenticate('my-strategy')
+        async whoAmI() {}
+      }
+
+      const test: TestClass = new TestClass();
+
+      const metaData = getAuthenticateMetadata(test, 'whoAmI');
+      const expectedMetaDataObject = {strategy: 'my-strategy', options: {}};
+      expect(metaData).to.deepEqual(expectedMetaDataObject);
+    });
   });
 });
