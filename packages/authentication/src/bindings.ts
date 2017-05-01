4e67864c52bc55d5ab397894248e3f8f3f57d495
deepakrkris <deepak.r.kris@gmail.com>
2017-05-01 07:10:21 -0700

    Implement passport strategy adapter
    * Add an adapter to execute all passport strategies
    
    * Add an interface to define all passport strategies
    
    * Change getAuthenticatedUser binding to async function

--- packages/authentication/src/bindings.ts
+++ packages/authentication/src/bindings.ts
@@ -4,25 +4,27 @@
 // License text available at https://opensource.org/licenses/MIT
 
 import * as http from 'http';
-import {Authentication} from './authenticate';
+import {Strategy} from './strategy-adapter';
+import {StrategyAdapter} from './strategy-adapter';
 
 /**
- * Authenticates http request using basic strategy and returns authenticated 
- * user. If user is not authenticated, http 401 Access denied is sent to the 
+ * Authenticates http request using basic strategy and returns authenticated
+ * user. If user is not authenticated, http 401 Access denied is sent to the
  * client.
  * @param required {boolean} Authentication required?
  * @param res {http.ServerRequest} The incoming http request.
  * @param strategy {Authentication} Authentication wrapper for Basic Strategy.
  */
-export const getAuthenticatedUser = (
-  required: boolean, 
+export async function getAuthenticatedUser(
+  required: boolean,
   req: http.ServerRequest,
-  strategy: Authentication
- ) => {
-    const user =  strategy.getAuthenticatedUser(req);
+  strategy: Strategy,
+ ): Promise<Object> {
+    const command = new StrategyAdapter(strategy);
+    const user =  await command.authenticate(req);
     if (required && !user) throw createAuthError(401, 'Access is denied due to invalid credentials');
     return user;
-};
+}
 
 interface HttpError extends Error {
   statusCode?: number;
