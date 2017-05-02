aaea6b3d9a1fa003ad69806a63383eb685cb075a
rashmihunt <hrashmi@us.ibm.com>
2017-05-01 22:34:03 -0700

    Initial bindings impl

---
+++ packages/authentication/src/bindings.ts
@@ -0,0 +1,38 @@
+// Copyright IBM Corp. 2013,2017. All Rights Reserved.
+// Node module: loopback
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import * as http from 'http';
+import {Strategy} from './strategy-adapter';
+import {StrategyAdapter} from './strategy-adapter';
+
+/**
+ * Authenticates http request using basic strategy and returns authenticated
+ * user. If user is not authenticated, http 401 Access denied is sent to the
+ * client.
+ * @param required {boolean} Authentication required?
+ * @param res {http.ServerRequest} The incoming http request.
+ * @param strategy {Authentication} Authentication wrapper for Basic Strategy.
+ */
+export async function getAuthenticatedUser(
+  required: boolean,
+  req: http.ServerRequest,
+  strategy: Strategy,
+ ): Promise<Object> {
+  const command = new StrategyAdapter(strategy);
+  const user =  await command.authenticate(req);
+  if (required && !user) throw createAuthError(401, 'Access is denied due to invalid credentials');
+  return user;
+}
+
+interface HttpError extends Error {
+  statusCode?: number;
+  status?: number;
+}
+
+function createAuthError(statusCode: number, message: string) {
+  const err = new Error(message) as HttpError;
+  err.statusCode = statusCode;
+  return err;
+}
--- packages/loopback/lib/application.ts
+++ packages/loopback/lib/application.ts
@@ -7,6 +7,7 @@ import {Binding, Context, Constructor} from '@loopback/context';
 import * as http from 'http';
 import {SwaggerRouter} from './router/SwaggerRouter';
 import {getApiSpec} from './router/metadata';
+import {getAuthenticatedUser, Strategy} from '../../authentication';
 
 const debug = require('debug')('loopback:Application');
 
@@ -25,6 +26,8 @@ export class Application extends Context {
         requestContext.bind('controller.current.operation').to(operationName);
         requestContext.bind('http.request').to(req);
         requestContext.bind('http.response').to(res);
+        //[rashmi] TODO calling authenticate() here only temporary until Middle design/implementation is in place 
+        this.authenticate(req);
         return requestContext.get(b.key);
       };
       const apiSpec = getApiSpec(ctor);
@@ -48,5 +51,16 @@ export class Application extends Context {
    */
   public controller<T>(controllerCtor: Constructor<T>): Binding {
     return this.bind('controllers.' + controllerCtor.name).toClass(controllerCtor);
+  }
+
+  public authenticate(req: http.ServerRequest) {
+    const strategy = this.get('authentication.strategy');
+    //for now, we assume requeired is always true for now.
+    getAuthenticatedUser(true, req, strategy)
+      .then((user : object) => {
+        this.bind('authentication.user').to(user);
+      }).catch((err: Error) => {
+        throw err; //[rashmi] TODO re-throw here?
+      });
   }
 }
