198f264d5717f86c6d03ecaf410a8aa50f80486a
Simon Ho <simonho@ca.ibm.com>
2017-05-02 19:16:21 -0700

    Fix copyright comments in @loopback/core

--- packages/core/src/router/metadata.ts
+++ packages/core/src/router/metadata.ts
@@ -1,5 +1,5 @@
-// Copyright IBM Corp. 2013,2017. All Rights Reserved.
-// Node module: loopback
+// Copyright IBM Corp. 2017. All Rights Reserved.
+// Node module: @loopback/core
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
