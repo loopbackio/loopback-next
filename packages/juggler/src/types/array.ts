9c5a514a9cea06e0c97c5503dc18ef35a6d9c8c9
Raymond Feng <rfeng@us.ibm.com>
2017-05-22 10:25:52 -0700

    Implement @repository for CDI

--- packages/juggler/src/types/array.ts
+++ packages/juggler/src/types/array.ts
@@ -24,7 +24,7 @@ export class ArrayType<T> implements Type<Array<T>> {
     return value.every((i) => this.itemType.isCoercible(i));
   }
 
-  defaultValue() {
+  defaultValue(): Array<T> {
     return [];
   }
 
