232c9003c10c22174a7973a11003c44fc53b26eb
Miroslav Bajtoš <mbajtos@cz.ibm.com>
2017-05-23 16:03:12 +0200

    Rework SwaggerRouter tests to use supertest

--- packages/core/test/support/util.ts
+++ packages/core/test/support/util.ts
@@ -24,23 +24,3 @@ export function createServer(config?: ServerConfig) : Server {
 export function getContext() : Context {
   return new Context();
 }
-
-/**
- * Tell "server" to start listening and wait until it listens.
- * Return a promise that's resolved with the base URL where the
- * server is listening at.
- */
-export async function listen(server: http.Server,
-    port: number = 0, host: string = '127.0.0.1'): Promise<string> {
-
-  if (port !== 0) {
-    console.warn('Do not use a hard-coded port number in the tests, it makes them fragile.');
-  }
-
-  // NOTE(bajtos) bluebird.promisify looses type information about the original function
-  // As a (temporary?) workaround, I am casting the result to "any function"
-  // This would be a more accurate type: (port: number) => Promise<http.Server>
-  const listen = bluebird.promisify(server.listen, { context: server }) as Function;
-  await listen(port, host);
-  return 'http://' + host + ':' + server.address().port;
-}
