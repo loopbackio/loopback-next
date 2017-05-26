438b413a6dd8b338fc625bd60d9a74f27047a9f4
Miroslav Bajtoš <mbajtoss@gmail.com>
2017-05-26 22:18:32 +0200

    Add HTTP Request/Response mocks to testlab (#311)
    * testlab: move supertest utilities to new file
    
    * testlab: add HTTP Request/Response mocks

---
+++ packages/testlab/src/shot.ts
@@ -0,0 +1,33 @@
+// Copyright IBM Corp. 2013,2017. All Rights Reserved.
+// Node module: @loopback/testlab
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+/*
+ * HTTP Request/Response mocks
+ * https://github.com/hapijs/shot
+ */
+
+import {ServerRequest, ServerResponse} from 'http';
+
+import {
+  RequestOptions as ShotRequestOptions,
+  ResponseObject,
+  inject} from 'shot';
+
+export {inject, ShotRequestOptions, ResponseObject};
+
+// tslint:disable-next-line:variable-name
+export const ShotRequest: ShotRequestCtor = require('shot/lib/request');
+
+// tslint:disable-next-line:variable-name
+export const ShotResponse: ShotResponseCtor = require('shot/lib/response');
+
+export type ShotRequestCtor =
+  new(options: ShotRequestOptions) => ServerRequest;
+
+export type ShotCallback = (response: ResponseObject) => void;
+
+export type ShotResponseCtor =
+  new(request: ServerRequest, onEnd: ShotCallback) => ServerResponse;
+
