2d4679dbbb9b9d6d22f12dfcad816f23b9f97aae
Raymond Feng <rfeng@us.ibm.com>
2017-05-30 13:22:33 -0700

    Update tsconfig for juggler

---
+++ packages/juggler/src/index.ts
@@ -0,0 +1,13 @@
+// Copyright IBM Corp. 2017. All Rights Reserved.
+// Node module: @loopback/juggler
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+export * from './common';
+export * from './decorator';
+export * from './model';
+export * from './query';
+export * from './connector';
+export * from './datasource';
+export * from './mixin';
+export * from './repository';
