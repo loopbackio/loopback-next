f6b4663d82340ecd2d94e3478e6dbbda864fc543
Mahesh Patsute <mahesh_patsute@yahoo.com>
2017-06-28 19:57:57 -0700

    Trying to fix build error

--- bin/generate-apidocs.js
+++ bin/generate-apidocs.js
@@ -1,6 +1,7 @@
 'use strict';
+const spawn = require('child_process').spawn;
 // Call sdocs
-require('child_process').spawn(
+spawn(
   '../../node_modules/strong-docs/bin/cli.js',
   ['-o', 'api-docs']
 ).on('close', (number, signal) => (process.exitCode = number));
