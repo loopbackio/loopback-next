788b54e5390840440f96f8dffe8cb94e1f1ca4ca
Mahesh Patsute <mahesh_patsute@yahoo.com>
2017-06-28 20:30:47 -0700

    trying to fix CI

--- bin/generate-apidocs.js
+++ bin/generate-apidocs.js
@@ -2,6 +2,6 @@
 const spawn = require('child_process').spawn;
 // Call sdocs
 spawn(
-  '../../node_modules/strong-docs/bin/cli.js',
-  ['-o', 'api-docs']
+  process.execPath,
+  ['../../node_modules/strong-docs/bin/cli.js', '-o', 'api-docs']
 ).on('close', (number, signal) => (process.exitCode = number));
