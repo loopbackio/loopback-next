e1cd6e804c41321a7bee5eb53c1d9211bdb2132f
Mahesh Patsute <mahesh_patsute@yahoo.com>
2017-07-05 11:39:15 -0700

    now using require.resolve

--- bin/generate-apidocs.js
+++ bin/generate-apidocs.js
@@ -3,5 +3,5 @@ const spawn = require('child_process').spawn;
 // Call sdocs
 spawn(
   process.execPath,
-  ['../../node_modules/strong-docs/bin/cli.js', '-o', 'api-docs']
+  [require.resolve('strong-docs/bin/cli'), '-o', 'api-docs']
 ).on('close', (number, signal) => (process.exitCode = number));
