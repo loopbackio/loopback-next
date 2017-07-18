13cf3079c3926ba32fe80cc960a2f680c1adaaa6
Miroslav Bajtoš <mbajtoss@gmail.com>
2017-07-18 16:34:32 +0200

    apidocs: fix the script to forward stdout/stderr
    Fix the script running strong-docs to generate api-docs, so that it
    correctly forwards strong-doc's stdout and stderr.

--- bin/generate-apidocs.js
+++ bin/generate-apidocs.js
@@ -3,5 +3,6 @@ const spawn = require('child_process').spawn;
 // Call sdocs
 spawn(
   process.execPath,
-  [require.resolve('strong-docs/bin/cli'), '-o', 'api-docs']
+  [require.resolve('strong-docs/bin/cli'), '-o', 'api-docs'],
+  {stdio: 'inherit'}
 ).on('close', (number, signal) => (process.exitCode = number));
