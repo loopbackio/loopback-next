d4ee86a4c040383a09b8e3897afe72df803e95ac
Taranveer Virk <taranveer@virk.cc>
2017-09-27 04:17:09 -0400

    ci: update tense rules to warn not fail on body and footer (#591)

--- commitlint.config.js
+++ commitlint.config.js
@@ -11,8 +11,8 @@ module.exports = {
   rules: {
     'header-max-length': [2, 'always', 100],
     'footer-max-length': [2, 'always', 100],
-    'body-tense': [2, 'always', ['present-imperative']],
-    'footer-tense': [2, 'always', ['present-imperative']],
+    'body-tense': [1, 'always', ['present-imperative']],
+    'footer-tense': [1, 'always', ['present-imperative']],
     'subject-tense': [2, 'always', ['present-imperative']],
     'lang': [0, 'always', 'eng'],
     'body-leading-blank': [2, 'always'],
