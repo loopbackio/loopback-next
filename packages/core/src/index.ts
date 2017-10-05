fe2287467694ee9b02a8f55ac469e5b7b866541b
Taranveer Virk <taranveer@virk.cc>
2017-10-04 20:10:36 -0400

    refactor(core): export `Constructor` from `@loopback/context`
    Enables it's use in extension / application development without a dependency on @loopback/context

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -7,7 +7,7 @@
 export {Component, ProviderMap} from './component';
 
 // loopback dependencies
-export {inject, Context} from '@loopback/context';
+export {inject, Context, Constructor} from '@loopback/context';
 export {Server} from './server';
 export * from './application';
 export * from './promisify';
