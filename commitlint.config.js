2c60b1ece5004be3603b6b1bce38c299c47d09ff
Raymond Feng <rfeng@us.ibm.com>
2017-11-08 14:20:20 -0800

    fix: Remove deprecated rules for commitlint
    See https://github.com/marionebl/commitlint/issues/54

--- commitlint.config.js
+++ commitlint.config.js
@@ -11,10 +11,6 @@ module.exports = {
   rules: {
     'header-max-length': [2, 'always', 100],
     'footer-max-length': [2, 'always', 100],
-    'body-tense': [1, 'always', ['present-imperative']],
-    'footer-tense': [1, 'always', ['present-imperative']],
-    'subject-tense': [2, 'always', ['present-imperative']],
-    'lang': [0, 'always', 'eng'],
     'body-leading-blank': [2, 'always'],
     'footer-leading-blank': [0, 'always'],
   }
