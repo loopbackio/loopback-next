e6f163800609b9b31854e622ea5828782f684aff
Raymond Feng <enjoyjava@gmail.com>
2017-11-08 09:50:23 -0800

    fix: Fix lint errors by newer version of prettier

--- packages/repository/src/repository-mixin.ts
+++ packages/repository/src/repository-mixin.ts
@@ -9,15 +9,15 @@ import {Repository} from './repository';
 // tslint:disable:no-any
 
 /**
-* A mixin class for Application that creates a .repository()
-* function to register a repository automatically. Also overrides
-* component function to allow it to register repositories automatically.
-*
-* ```ts
-*
-* class MyApplication extends RepositoryMixin(Application) {}
-* ```
-*/
+ * A mixin class for Application that creates a .repository()
+ * function to register a repository automatically. Also overrides
+ * component function to allow it to register repositories automatically.
+ *
+ * ```ts
+ *
+ * class MyApplication extends RepositoryMixin(Application) {}
+ * ```
+ */
 export function RepositoryMixin<T extends Class<any>>(superClass: T) {
   return class extends superClass {
     // A mixin class has to take in a type any[] argument!
@@ -40,69 +40,69 @@ export function RepositoryMixin<T extends Class<any>>(superClass: T) {
     }
 
     /**
-    * Add a repository to this application.
-    *
-    * @param repo The repository to add.
-    *
-    * ```ts
-    *
-    * class NoteRepo {
-    *   model: any;
-    *
-    *   constructor() {
-    *     const ds: juggler.DataSource = new DataSourceConstructor({
-    *       name: 'db',
-    *       connector: 'memory',
-    *     });
-    *
-    *     this.model = ds.createModel(
-    *       'note',
-    *       {title: 'string', content: 'string'},
-    *       {}
-    *     );
-    *   }
-    * };
-    *
-    * app.repository(NoteRepo);
-    * ```
-    */
+     * Add a repository to this application.
+     *
+     * @param repo The repository to add.
+     *
+     * ```ts
+     *
+     * class NoteRepo {
+     *   model: any;
+     *
+     *   constructor() {
+     *     const ds: juggler.DataSource = new DataSourceConstructor({
+     *       name: 'db',
+     *       connector: 'memory',
+     *     });
+     *
+     *     this.model = ds.createModel(
+     *       'note',
+     *       {title: 'string', content: 'string'},
+     *       {}
+     *     );
+     *   }
+     * };
+     *
+     * app.repository(NoteRepo);
+     * ```
+     */
     repository(repo: Class<Repository<any>>) {
       const repoKey = `repositories.${repo.name}`;
       this.bind(repoKey).toClass(repo);
     }
 
     /**
-    * Add a component to this application. Also mounts
-    * all the components repositories.
-    *
-    * @param component The component to add.
-    *
-    * ```ts
-    *
-    * export class ProductComponent {
-    *   controllers = [ProductController];
-    *   repositories = [ProductRepo, UserRepo];
-    *   providers = {
-    *     [AUTHENTICATION_STRATEGY]: AuthStrategy,
-    *     [AUTHORIZATION_ROLE]: Role,
-    *   };
-    * };
-    *
-    * app.component(ProductComponent);
-    * ```
-    */
+     * Add a component to this application. Also mounts
+     * all the components repositories.
+     *
+     * @param component The component to add.
+     *
+     * ```ts
+     *
+     * export class ProductComponent {
+     *   controllers = [ProductController];
+     *   repositories = [ProductRepo, UserRepo];
+     *   providers = {
+     *     [AUTHENTICATION_STRATEGY]: AuthStrategy,
+     *     [AUTHORIZATION_ROLE]: Role,
+     *   };
+     * };
+     *
+     * app.component(ProductComponent);
+     * ```
+     */
     public component(component: Class<any>) {
       super.component(component);
       this.mountComponentRepository(component);
     }
 
     /**
-    * Get an instance of a component and mount all it's
-    * repositories. This function is intended to be used internally
-    * by component()
-    *
-    * @param component The component to mount repositories of
-    */
+     * Get an instance of a component and mount all it's
+     * repositories. This function is intended to be used internally
+     * by component()
+     *
+     * @param component The component to mount repositories of
+     */
     mountComponentRepository(component: Class<any>) {
       const componentKey = `components.${component.name}`;
       const compInstance = this.getSync(componentKey);
