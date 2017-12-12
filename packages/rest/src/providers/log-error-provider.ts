6a3a1a22266f917929d0a46080468e11f3e8d234
Miroslav Bajtoš <mbajtoss@gmail.com>
2017-12-12 07:56:21 +0100

    refactor(rest): use named functions for sequence actions (#790)
    Refactor sequence-action providers to use a named "action" method
    to hold the actual action implementation, and change the anonymous
    lambda (arrow) functions returned by "value" methods to delegate
    to these new "action" methods.
    
    This improves error stack traces in cases where the action fails.
    Before this change, there would be no reference to the actual sequence
    action that failed. After this change, the stack trace contains
    an entry for "{Action}Provider.action" function.

--- packages/rest/src/providers/log-error-provider.ts
+++ packages/rest/src/providers/log-error-provider.ts
@@ -3,21 +3,26 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-import {BoundValue, Provider} from '@loopback/context';
+import {Provider} from '@loopback/context';
 import {ServerRequest} from '../';
+import {LogError} from '../internal-types';
 
-export class LogErrorProvider implements Provider<BoundValue> {
-  value() {
-    return (err: Error, statusCode: number, req: ServerRequest) => {
-      if (statusCode >= 500) {
-        console.error(
-          'Unhandled error in %s %s: %s %s',
-          req.method,
-          req.url,
-          statusCode,
-          err.stack || err,
-        );
-      }
-    };
+export class LogErrorProvider implements Provider<LogError> {
+  value(): LogError {
+    return (err, statusCode, req) => this.action(err, statusCode, req);
+  }
+
+  action(err: Error, statusCode: number, req: ServerRequest) {
+    if (statusCode < 500) {
+      return;
+    }
+
+    console.error(
+      'Unhandled error in %s %s: %s %s',
+      req.method,
+      req.url,
+      statusCode,
+      err.stack || err,
+    );
   }
 }
--- packages/rest/src/providers/reject.ts
+++ packages/rest/src/providers/reject.ts
@@ -4,24 +4,26 @@
 // License text available at https://opensource.org/licenses/MIT
 
 import {LogError, Reject} from '../internal-types';
-import {inject} from '@loopback/context';
+import {inject, Provider} from '@loopback/context';
 import {ServerResponse, ServerRequest} from 'http';
 import {HttpError} from 'http-errors';
 import {writeErrorToResponse} from '../writer';
 import {RestBindings} from '../keys';
 
-export class RejectProvider {
+export class RejectProvider implements Provider<Reject> {
   constructor(
     @inject(RestBindings.SequenceActions.LOG_ERROR)
     protected logError: LogError,
   ) {}
 
   value(): Reject {
-    return (response: ServerResponse, request: ServerRequest, error: Error) => {
-      const err = <HttpError>error;
-      const statusCode = err.statusCode || err.status || 500;
-      writeErrorToResponse(response, err);
-      this.logError(error, statusCode, request);
-    };
+    return (response, request, error) => this.action(response, request, error);
+  }
+
+  action(response: ServerResponse, request: ServerRequest, error: Error) {
+    const err = <HttpError>error;
+    const statusCode = err.statusCode || err.status || 500;
+    writeErrorToResponse(response, err);
+    this.logError(error, statusCode, request);
   }
 }
