647476348d20924d5acbd1ca3bbf6baf7added95
Taranveer Virk <taranveer@virk.cc>
2018-01-02 19:15:57 -0500

    test: run cli tests in top level npm test (#825)
    * test: run cli tests in top level npm test
    
    * fix(cli): use path.sep to be platform agnostic

--- packages/cli/test/test-utils.js
+++ packages/cli/test/test-utils.js
@@ -6,11 +6,12 @@
 'use strict';
 
 const yeoman = require('yeoman-environment');
+const path = require('path');
 
 exports.testSetUpGen = function(genName, arg) {
   arg = arg || {};
   const env = yeoman.createEnv();
-  const name = genName.substring(genName.lastIndexOf('/') + 1);
+  const name = genName.substring(genName.lastIndexOf(path.sep) + 1);
   env.register(genName, 'loopback4:' + name);
   return env.create('loopback4:' + name, arg);
 };
