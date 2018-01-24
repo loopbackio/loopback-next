38ea1fd68f86afa53d5e2ac396b54d2e620d0d54
jannyHou <juehou@ca.ibm.com>
2018-01-24 15:11:19 -0500

    fix: restore index.js

--- packages/testlab/index.js
+++ packages/testlab/index.js
@@ -1,11 +1,9 @@
-"use strict";
 // Copyright IBM Corp. 2017,2018. All Rights Reserved.
 // Node module: @loopback/testlab
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-exports.__esModule = true;
-// NOTE(bajtos) This file is used by VSCode/TypeScriptServer at dev time only
-__export(require("./src/testlab"));
+
+const nodeMajorVersion = +process.versions.node.split('.')[0];
+module.exports = nodeMajorVersion >= 7 ?
+  require('./dist/src/testlab') :
+  require('./dist6/src/testlab');
