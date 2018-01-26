a2d5e63b958d116594affb097d64c316a0319082
Miroslav Bajtoš <mbajtoss@gmail.com>
2018-01-26 14:11:45 +0100

    fixup! address review comments

---
+++ packages/testlab/src/index.ts
@@ -0,0 +1,11 @@
+// Copyright IBM Corp. 2018. All Rights Reserved.
+// Node module: @loopback/testlab
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+export * from './expect';
+export * from './sinon';
+export * from './client';
+export * from './shot';
+export * from './validate-api-spec';
+export * from './test-sandbox';
---
+++ packages/testlab/src/sinon.ts
@@ -0,0 +1,9 @@
+// Copyright IBM Corp. 2018. All Rights Reserved.
+// Node module: @loopback/testlab
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import sinon = require('sinon');
+import {SinonSpy} from 'sinon';
+
+export {sinon, SinonSpy};
