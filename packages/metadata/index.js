2b4f8226580bd1cb9f5c63c23c20828e3b30aa4a
biniam <biniam@ca.ibm.com>
2018-01-30 13:26:27 -0500

    build: drop dist6 related targets
    BREAKING CHANGE: Support for Node.js version lower than 8.0 has been dropped.
    Please upgrade to the latest Node.js 8.x LTS version.
    
    Co-Authored-by: Taranveer Virk <taranveer@virk.cc>

--- packages/metadata/index.js
+++ packages/metadata/index.js
@@ -3,5 +3,4 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-const nodeMajorVersion = +process.versions.node.split('.')[0];
-module.exports = nodeMajorVersion >= 7 ? require('./dist') : require('./dist6');
+module.exports = require('./dist');
