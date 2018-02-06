a2368cee598c131a826ee42e347266c6e25ae582
Biniam Admikew <binadmt@hotmail.com>
2018-02-06 00:48:49 -0500

    build: drop dist6 related targets (#945)
    BREAKING CHANGE: Support for Node.js version lower than 8.0 has been dropped.
    Please upgrade to the latest Node.js 8.x LTS version.
    
    Co-Authored-by: Taranveer Virk <taranveer@virk.cc>

--- packages/cli/generators/extension/templates/index.js
+++ packages/cli/generators/extension/templates/index.js
@@ -3,6 +3,4 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-const nodeMajorVersion = +process.versions.node.split('.')[0];
-const dist = nodeMajorVersion >= 7 ? './dist' : './dist6';
-module.exports = require(dist);
+module.exports = require('./dist');
--- packages/example-rpc-server/index.js
+++ packages/example-rpc-server/index.js
@@ -3,10 +3,7 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-const nodeMajorVersion = +process.versions.node.split('.')[0];
-const dist = nodeMajorVersion >= 7 ? './dist' : './dist6';
-
-const application = (module.exports = require(dist));
+const application = (module.exports = require('./dist'));
 
 if (require.main === module) {
   // Run the application
