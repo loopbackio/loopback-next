3f085fe78ed03d89aaa0da3bba4f5c59e8cfc2b6
Taranveer Virk <taranveer@virk.cc>
2018-04-09 20:15:31 -0400

    test(openapi-v3-types): improve test coverage
    connected to #181

--- packages/testlab/src/validate-api-spec.ts
+++ packages/testlab/src/validate-api-spec.ts
@@ -3,13 +3,13 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-import {OpenApiSpec} from '@loopback/openapi-v3-types';
 const validator = require('swagger2openapi/validate.js');
 import {promisify} from 'util';
 
 const validateAsync = promisify(validator.validate);
 
-export async function validateApiSpec(spec: OpenApiSpec): Promise<void> {
+// tslint:disable-next-line:no-any
+export async function validateApiSpec(spec: any): Promise<void> {
   const opts = {};
 
   try {
