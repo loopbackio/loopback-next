532f15324f0378a951dbb7f101ce9df3bb3711ef
Miroslav Bajtoš <mbajtoss@gmail.com>
2018-05-02 09:15:35 +0200

    feat: add helper package "dist-util"
    Extract code dealing with Node.js versions and different `dist` folders
    from packages' `index.js` files into a shared `@loopback/dist-util`
    package.

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -3,9 +3,7 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-const nodeMajorVersion = +process.versions.node.split('.')[0];
-const dist = nodeMajorVersion >= 10 ? './dist10' : './dist8';
-const application = require(dist);
+const application = require('@loopback/dist-util').loadDist(__dirname);
 
 module.exports = application;
 
---
+++ packages/dist-util/index.js
@@ -0,0 +1,25 @@
+// Copyright IBM Corp. 2018. All Rights Reserved.
+// Node module: @loopback/dist-util
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+'use strict';
+
+const path = require('path');
+
+function getDist() {
+  const nodeMajorVersion = +process.versions.node.split('.')[0];
+  if (nodeMajorVersion < 8) {
+    throw new Error(
+      `Node.js version ${process.versions.node} is not supported.` +
+      'Please use Node.js 8.x or newer.');
+  }
+  return nodeMajorVersion >= 10 ? './dist10' : './dist8';
+}
+
+function loadDist(projectRootDir) {
+  const dist = getDist();
+  return require(path.join(projectRootDir, dist));
+}
+
+module.exports = {getDist, loadDist};
