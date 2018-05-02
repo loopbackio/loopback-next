10d814cb5a62fe16f1ad78e0b3af73797b2053d5
Miroslav Bajtoš <mbajtoss@gmail.com>
2018-05-02 09:15:35 +0200

    feat: add helper package "dist-util"
    Extract code dealing with Node.js versions and different `dist` folders
    from packages' `index.js` files into a shared `@loopback/dist-util`
    package.

--- packages/repository/index.js
+++ packages/repository/index.js
@@ -3,6 +3,4 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-const nodeMajorVersion = +process.versions.node.split('.')[0];
-const dist = nodeMajorVersion >= 10 ? './dist10' : './dist8';
-module.exports = require(dist);
+module.exports = require('@loopback/dist-util').loadDist(__dirname);
