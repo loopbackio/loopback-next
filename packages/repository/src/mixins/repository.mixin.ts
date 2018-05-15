aff1e5b120edf42bb6755606a8a6135e7dc6d9c1
jannyHou <juehou@ca.ibm.com>
2018-05-15 12:12:00 -0400

    fix: move apidocs outside of the function

--- packages/repository/src/mixins/repository.mixin.ts
+++ packages/repository/src/mixins/repository.mixin.ts
@@ -17,6 +17,10 @@ import {Application} from '@loopback/core';
  *
  * class MyApplication extends RepositoryMixin(Application) {}
  * ```
+ *
+ * Please note: the members in the mixin function are documented in a dummy class
+ * called <a href="#RepositoryMixinDoc">RepositoryMixinDoc</a>
+ *
  */
 // tslint:disable-next-line:no-any
 export function RepositoryMixin<T extends Class<any>>(superClass: T) {
@@ -154,4 +158,114 @@ export interface AppWithRepository extends Application {
   dataSource(dataSource: juggler.DataSource, name?: string): void;
   component(component: Class<{}>): void;
   mountComponentRepository(component: Class<{}>): void;
+}
+
+/**
+ * A dummy class created to generate the tsdoc for the members in repository
+ * mixin. Please don't use it.
+ *
+ * The members are implemented in function
+ * <a href="#RepositoryMixin">RepositoryMixin</a>
+ */
+export class RepositoryMixinDoc {
+  // tslint:disable-next-line:no-any
+  constructor(...args: any[]) {
+    throw new Error(
+      'This is a dummy class created for apidoc!' + 'Please do not use it!',
+    );
+  }
+
+  /**
+   * Add a repository to this application.
+   *
+   * @param repo The repository to add.
+   *
+   * ```ts
+   *
+   * class NoteRepo {
+   *   model: any;
+   *
+   *   constructor() {
+   *     const ds: juggler.DataSource = new juggler.DataSource({
+   *       name: 'db',
+   *       connector: 'memory',
+   *     });
+   *
+   *     this.model = ds.createModel(
+   *       'note',
+   *       {title: 'string', content: 'string'},
+   *       {}
+   *     );
+   *   }
+   * };
+   *
+   * app.repository(NoteRepo);
+   * ```
+   */
+  // tslint:disable-next-line:no-any
+  repository(repo: Class<Repository<any>>): void {}
+
+  /**
+   * Retrieve the repository instance from the given Repository class
+   *
+   * @param repo The repository class to retrieve the instance of
+   */
+  // tslint:disable-next-line:no-any
+  async getRepository<R extends Repository<any>>(repo: Class<R>): Promise<R> {
+    return new repo() as R;
+  }
+
+  /**
+   * Add the dataSource to this application.
+   *
+   * @param dataSource The dataSource to add.
+   * @param name The binding name of the datasource; defaults to dataSource.name
+   *
+   * ```ts
+   *
+   * const ds: juggler.DataSource = new juggler.DataSource({
+   *   name: 'db',
+   *   connector: 'memory',
+   * });
+   *
+   * app.dataSource(ds);
+   *
+   * // The datasource can be injected with
+   * constructor(@inject('datasources.db') protected datasource: DataSourceType) {
+   *
+   * }
+   * ```
+   */
+  dataSource(dataSource: juggler.DataSource, name?: string) {}
+
+  /**
+   * Add a component to this application. Also mounts
+   * all the components repositories.
+   *
+   * @param component The component to add.
+   *
+   * ```ts
+   *
+   * export class ProductComponent {
+   *   controllers = [ProductController];
+   *   repositories = [ProductRepo, UserRepo];
+   *   providers = {
+   *     [AUTHENTICATION_STRATEGY]: AuthStrategy,
+   *     [AUTHORIZATION_ROLE]: Role,
+   *   };
+   * };
+   *
+   * app.component(ProductComponent);
+   * ```
+   */
+  public component(component: Class<{}>) {}
+
+  /**
+   * Get an instance of a component and mount all it's
+   * repositories. This function is intended to be used internally
+   * by component()
+   *
+   * @param component The component to mount repositories of
+   */
+  mountComponentRepository(component: Class<{}>) {}
 }
