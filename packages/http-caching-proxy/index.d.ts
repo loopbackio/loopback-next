9488dfc0617abba4d2dcb138885ac0850c07867f
Miroslav Bajtoš <mbajtoss@gmail.com>
2018-05-25 15:31:33 +0200

    feat(http-caching-proxy): initial implementation
    Implement an HTTP proxy with aggressive cache persisted on file system.
    This proxy is intended for integration tests to reduce the time needed
    to access real backends.

---
+++ packages/http-caching-proxy/index.d.ts
@@ -0,0 +1,6 @@
+// Copyright IBM Corp. 2018. All Rights Reserved.
+// Node module: @loopback/http-caching-proxy
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+export * from './dist8';
