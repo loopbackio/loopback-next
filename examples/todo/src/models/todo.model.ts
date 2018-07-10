9d5af7ea823d4b86adc88d67da3fd3b6b10f1497
virkt25 <taranveer@virk.cc>
2018-07-10 19:39:32 -0400

    refactor(example-todo): update files to match output from `lb4 model`

--- examples/todo/src/models/todo.model.ts
+++ examples/todo/src/models/todo.model.ts
@@ -3,7 +3,7 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-import {Entity, property, model} from '@loopback/repository';
+import {Entity, model, property} from '@loopback/repository';
 
 @model()
 export class Todo extends Entity {
@@ -27,22 +27,18 @@ export class Todo extends Entity {
   @property({
     type: 'boolean',
   })
-  isComplete: boolean;
+  isComplete?: boolean;
 
   @property({
     type: 'string',
   })
-  remindAtAddress: string; // address,city,zipcode
+  remindAtAddress?: string; // address,city,zipcode
 
   // TODO(bajtos) Use LoopBack's GeoPoint type here
   @property({
     type: 'string',
   })
-  remindAtGeo: string; // latitude,longitude
-
-  getId() {
-    return this.id;
-  }
+  remindAtGeo?: string; // latitude,longitude
 
   constructor(data?: Partial<Todo>) {
     super(data);
