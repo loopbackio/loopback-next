8ba0801d7f3a584781ac2c2a87d62aef8ae73318
Raymond Feng <enjoyjava@gmail.com>
2018-07-13 09:30:48 -0700

    feat(repository): add KVRepository impl using legacy juggler

--- packages/repository/src/repositories/kv.repository.ts
+++ packages/repository/src/repositories/kv.repository.ts
@@ -6,30 +6,38 @@
 import {Repository} from './repository';
 import {Options, DataObject} from '../common-types';
 import {Model} from '../model';
-import {Filter} from '../query';
+
+/**
+ * Filter for keys
+ */
+export type KeyValueFilter = {
+  /**
+   * Glob string to use to filter returned keys (i.e. `userid.*`). All
+   * connectors are required to support `*` and `?`. They may also support
+   * additional special characters that are specific to the backing database.
+   */
+  match: string;
+};
 
 /**
  * Key/Value operations for connector implementations
  */
-export interface KVRepository<T extends Model> extends Repository<T> {
+export interface KeyValueRepository<T extends Model> extends Repository<T> {
   /**
    * Delete an entry by key
    *
    * @param key Key for the entry
    * @param options Options for the operation
-   * @returns Promise<true> if an entry is deleted for the key, otherwise
-   * Promise<false>
    */
-  delete(key: string, options?: Options): Promise<boolean>;
+  delete(key: string, options?: Options): Promise<void>;
 
   /**
    * Delete all entries
    *
    * @param key Key for the entry
    * @param options Options for the operation
-   * @returns A promise of the number of entries deleted
    */
-  deleteAll(options?: Options): Promise<number>;
+  deleteAll(options?: Options): Promise<void>;
 
   /**
    * Get an entry by key
@@ -46,20 +54,16 @@ export interface KVRepository<T extends Model> extends Repository<T> {
    * @param key Key for the entry
    * @param value Value for the entry
    * @param options Options for the operation
-   * @returns Promise<true> if an entry is set for the key, otherwise
-   * Promise<false>
    */
-  set(key: string, value: DataObject<T>, options?: Options): Promise<boolean>;
+  set(key: string, value: DataObject<T>, options?: Options): Promise<void>;
 
   /**
    * Set up ttl for an entry by key
    *
    * @param key Key for the entry
    * @param options Options for the operation
-   * @returns Promise<true> if an entry is set for the key, otherwise
-   * Promise<false>
    */
-  expire(key: string, ttl: number, options?: Options): Promise<boolean>;
+  expire(key: string, ttl: number, options?: Options): Promise<void>;
 
   /**
    * Get ttl for an entry by key
@@ -68,23 +72,15 @@ export interface KVRepository<T extends Model> extends Repository<T> {
    * @param options Options for the operation
    * @returns A promise of the TTL value
    */
-  ttl?(key: string, ttl: number, options?: Options): Promise<number>;
-
-  /**
-   * Fetch all keys
-   *
-   * @param key Key for the entry
-   * @param options Options for the operation
-   * @returns A promise of an array of keys for all entries
-   */
-  keys?(options?: Options): Promise<string[]>;
+  ttl?(key: string, options?: Options): Promise<number>;
 
   /**
    * Get an Iterator for matching keys
    *
    * @param filter Filter for keys
    * @param options Options for the operation
-   * @returns A promise of an iterator of entries
+   * @returns An async iteratable iterator of keys so that the return value can
+   * be used with `for-await-of`.
    */
-  iterateKeys?(filter?: Filter, options?: Options): Promise<Iterator<T>>;
+  keys?(filter?: KeyValueFilter, options?: Options): AsyncIterable<string>;
 }
