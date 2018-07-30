e19396f6211814dbee73a754de8011ec48f40b93
Hage Yaapa <hage.yaapa@in.ibm.com>
2018-07-30 18:18:08 +0530

    chore: todo example on IBM Cloud
    add todo example on IBM Cloud

--- examples/todo/src/datasources/db.datasource.ts
+++ examples/todo/src/datasources/db.datasource.ts
@@ -3,9 +3,20 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
+import process = require('process');
 import {inject} from '@loopback/core';
 import {juggler, DataSource} from '@loopback/repository';
 const config = require('./db.datasource.json');
+
+if (shouldConfigureForIbmCloud()) {
+  const services = JSON.parse(process.env.VCAP_SERVICES!);
+  Object.keys(services).forEach(key => {
+    const service = services[key];
+    if (service.name === config.name) {
+      config.url = service.url;
+    }
+  });
+}
 
 export class DbDataSource extends juggler.DataSource {
   static dataSourceName = 'db';
@@ -16,4 +27,15 @@ export class DbDataSource extends juggler.DataSource {
   ) {
     super(dsConfig);
   }
+}
+
+function shouldConfigureForIbmCloud() {
+  if (
+    process.env.HOME === '/home/vcap' &&
+    config.ibmCloud &&
+    process.env.VCAP_SERVICES
+  ) {
+    return true;
+  }
+  return false;
 }
