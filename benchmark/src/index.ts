5066cdfc3af602369d63be76676b9153b3be34ef
Miroslav Bajtoš <mbajtoss@gmail.com>
2018-08-01 15:45:10 +0200

    feat: add two simple benchmarks

---
+++ benchmark/src/index.ts
@@ -0,0 +1,18 @@
+// Copyright IBM Corp. 2018. All Rights Reserved.
+// Node module: @loopback/benchmark
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {Benchmark, Options} from './benchmark';
+
+export {Benchmark, Options};
+
+export async function main() {
+  const duration = process.env.DURATION || '30';
+  const options: Options = {
+    duration: +duration,
+  };
+  const bench = new Benchmark(options);
+  bench.logger = (title, stats) => console.log('%s:', title, stats);
+  await bench.run();
+}
