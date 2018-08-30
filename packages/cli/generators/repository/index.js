99a0bade325fa2f77fbc4bd1aa1f1e1874c590a4
Mario Estrada <marioestradarosa@yahoo.com>
2018-08-30 10:37:49 -0600

    feat(cli): ast-helper integration

--- packages/cli/generators/repository/index.js
+++ packages/cli/generators/repository/index.js
@@ -6,84 +6,144 @@
 'use strict';
 const _ = require('lodash');
 const ArtifactGenerator = require('../../lib/artifact-generator');
+const fs = require('fs');
 const debug = require('../../lib/debug')('repository-generator');
 const inspect = require('util').inspect;
 const path = require('path');
 const chalk = require('chalk');
 const utils = require('../../lib/utils');
-const util = require('util');
-const fs = require('fs');
-const exists = util.promisify(fs.exists);
+const connectors = require('../datasource/connectors.json');
+const tsquery = require('../../lib/ast-helper');
 
-const SERVICE_VALUE_CONNECTOR = 'soap,rest';
-const KEY_VALUE_CONNECTOR = 'kv-';
+const VALID_CONNECTORS_FOR_REPOSITORY = ['KeyValueModel', 'PersistedModel'];
+const KEY_VALUE_CONNECTOR = ['KeyValueModel'];
 
-const KEY_VALUE_REPOSITORY = 'KeyValueRepository';
 const DEFAULT_CRUD_REPOSITORY = 'DefaultCrudRepository';
+const KEY_VALUE_REPOSITORY = 'DefaultKeyValueRepository';
 
 const REPOSITORY_KV_TEMPLATE = 'repository-kv-template.ts.ejs';
 const REPOSITORY_CRUD_TEMPLATE = 'repository-crud-default-template.ts.ejs';
 
 const PROMPT_MESSAGE_MODEL =
-  'Select the model you want to generate a repository';
+  'Select the model(s) you want to generate a repository';
 const PROMPT_MESSAGE_DATA_SOURCE = 'Please select the datasource';
-const PROMPT_MESSAGE_ID_TYPE = 'What is the type of your ID?';
-
 const ERROR_READING_FILE = 'Error reading file';
 const ERROR_NO_DATA_SOURCES_FOUND = 'No datasources found in';
 const ERROR_NO_MODELS_FOUND = 'No models found in';
+const ERROR_NO_MODEL_SELECTED = 'You did not select a valid model';
+const ERROR_NO_DIRECTORY = "couldn't find the directory";
 
 module.exports = class RepositoryGenerator extends ArtifactGenerator {
   // Note: arguments and options should be defined in the constructor.
   constructor(args, opts) {
     super(args, opts);
+  }
 
-    /** instance helper method isolated from the execution loop
-     * @connectorType: can be a single or a comma separated list
-     */
-    this.isConnectorType = async function(connectorType, dataSourceClassName) {
-      debug(`callling isConnectorType ${connectorType}`);
-      let jsonFileContent = '';
-      let result = false;
+  /**
+   * get the property name for the id field
+   * @param {string} modelName
+   */
+  async _getModelIdType(modelName) {
+    let fileContent = '';
+    let modelFile = path.join(
+      this.artifactInfo.modelDir,
+      `${utils.kebabCase(modelName)}.model.ts`,
+    );
+    try {
+      fileContent = this.fs.read(modelFile, {});
+    } catch (err) {
+      debug(`${ERROR_READING_FILE} ${modelFile}: ${err.message}`);
+      return this.exit(err);
+    }
 
-      let datasourceJSONFile = path.join(
-        'src',
-        'datasources',
-        dataSourceClassName
-          .replace('Datasource', '.datasource.json')
-          .toLowerCase(),
-      );
+    return tsquery.getIdFromModel(fileContent);
+  }
 
-      try {
-        const jsonFileExists = await exists(datasourceJSONFile);
-        if (jsonFileExists) {
-          jsonFileContent = this.fs.readJSON(datasourceJSONFile, {});
-        }
-      } catch (err) {
-        debug(`${ERROR_READING_FILE} ${datasourceJSONFile}: ${err}`);
-        return this.exit(err);
+  /**
+   * helper method to inspect and validate a repository type
+   */
+  async _inferRepositoryType() {
+    if (!this.artifactInfo.dataSourceClassName) {
+      return;
+    }
+    let result = this._isConnectorOfType(
+      KEY_VALUE_CONNECTOR,
+      this.artifactInfo.dataSourceClassName,
+    );
+    debug(`KeyValue Connector: ${result}`);
+
+    if (result) {
+      this.artifactInfo.repositoryTypeClass = KEY_VALUE_REPOSITORY;
+      this.artifactInfo.defaultTemplate = REPOSITORY_KV_TEMPLATE;
+    } else {
+      this.artifactInfo.repositoryTypeClass = DEFAULT_CRUD_REPOSITORY;
+      this.artifactInfo.defaultTemplate = REPOSITORY_CRUD_TEMPLATE;
+    }
+
+    // assign the data source name to the information artifact
+    let dataSourceName = this.artifactInfo.dataSourceClassName
+      .replace('Datasource', '')
+      .toLowerCase();
+    let dataSourceImportName = this.artifactInfo.dataSourceClassName.replace(
+      'Datasource',
+      'DataSource',
+    );
+
+    Object.assign(this.artifactInfo, {
+      dataSourceImportName: dataSourceImportName,
+    });
+    Object.assign(this.artifactInfo, {
+      dataSourceName: dataSourceName,
+    });
+  }
+
+  /**
+   * load the connectors available and check if the basedModel matches any
+   * connectorType supplied for the given connector name
+   * @param {string} connectorType single or a comma separated string array
+   */
+  _isConnectorOfType(connectorType, dataSourceClassName) {
+    debug(`calling isConnectorType ${connectorType}`);
+    let jsonFileContent = '';
+    let result = false;
+
+    if (!dataSourceClassName) {
+      return false;
+    }
+    let datasourceJSONFile = path.join(
+      this.artifactInfo.datasourcesDir,
+      dataSourceClassName
+        .replace('Datasource', '.datasource.json')
+        .toLowerCase(),
+    );
+
+    try {
+      jsonFileContent = this.fs.readJSON(datasourceJSONFile, {});
+    } catch (err) {
+      debug(`${ERROR_READING_FILE} ${datasourceJSONFile}: ${err.message}`);
+      return this.exit(err);
+    }
+
+    for (let connector of Object.values(connectors)) {
+      const matchedConnector =
+        jsonFileContent.connector === connector.name ||
+        jsonFileContent.connector === `loopback-connector-${connector.name}`;
+
+      if (matchedConnector && connectorType.includes(connector.baseModel)) {
+        result = true;
+        break;
       }
+    }
 
-      let keyWordsToSearch = connectorType.split(',');
-      for (let keyWord of keyWordsToSearch) {
-        debug(`asking for keyword ${keyWord}`);
-        if (jsonFileContent.connector.includes(keyWord)) {
-          result = true;
-          break;
-        }
-      }
-
-      return result;
-    };
+    return result;
   }
 
   _setupGenerator() {
-    super._setupGenerator();
-
     this.artifactInfo = {
-      type: 'repository',
+      type: 'datasource to use ',
       rootDir: 'src',
     };
+
     this.artifactInfo.outDir = path.resolve(
       this.artifactInfo.rootDir,
       'repositories',
@@ -96,19 +156,67 @@ module.exports = class RepositoryGenerator extends ArtifactGenerator {
       this.artifactInfo.rootDir,
       'models',
     );
+
+    this.artifactInfo.defaultTemplate = REPOSITORY_CRUD_TEMPLATE;
+
+    this.option('model', {
+      type: String,
+      required: false,
+      description: 'A valid model name',
+    });
+
+    this.option('id', {
+      type: String,
+      required: false,
+      description: 'A valid ID property name for the specified model',
+    });
+
+    return super._setupGenerator();
   }
 
   setOptions() {
     return super.setOptions();
   }
 
-  checkLoopBackProject() {
-    return super.checkLoopBackProject();
+  async checkPaths() {
+    // check for datasources
+    if (!fs.existsSync(this.artifactInfo.datasourcesDir)) {
+      return this.exit(
+        new Error(
+          `${ERROR_NO_DIRECTORY} ${this.artifactInfo.datasourcesDir}.
+        ${chalk.yellow(
+          'Please visit https://loopback.io/doc/en/lb4/DataSource-generator.html for information on how datasources are discovered',
+        )}`,
+        ),
+      );
+    }
+
+    // check for models
+    if (!fs.existsSync(this.artifactInfo.modelDir)) {
+      return this.exit(
+        new Error(
+          `${ERROR_NO_DIRECTORY} ${this.artifactInfo.modelDir}.
+        ${chalk.yellow(
+          'Please visit https://loopback.io/doc/en/lb4/Model-generator.html for information on how models are discovered',
+        )}`,
+        ),
+      );
+    }
   }
 
-  async promptDataSource() {
+  async promptDataSourceName() {
+    if (this.shouldExit()) return false;
+
     debug('Prompting for a datasource ');
+    let cmdDatasourceName;
     let datasourcesList;
+
+    // grab the datasourcename from the command line
+    cmdDatasourceName = this.options.name
+      ? utils.toClassName(this.options.name) + 'Datasource'
+      : '';
+
+    debug(`command line datasource is  ${cmdDatasourceName}`);
 
     try {
       datasourcesList = await utils.getArtifactList(
@@ -116,27 +224,40 @@ module.exports = class RepositoryGenerator extends ArtifactGenerator {
         'datasource',
         true,
       );
+      debug(`datasourcesList from src/datasources : ${datasourcesList}`);
     } catch (err) {
       return this.exit(err);
     }
 
-    // iterate over it to exclude service oriented data sources
-    let tempDataSourceList = Object.assign(datasourcesList, {});
-    for (let item of tempDataSourceList) {
-      let result = await this.isConnectorType(SERVICE_VALUE_CONNECTOR, item);
-      debug(`${item} has keyword ${SERVICE_VALUE_CONNECTOR} is ${result}`);
-      if (result) {
-        // remove from original list
-        _.remove(datasourcesList, e => e == item);
-      }
+    const availableDatasources = datasourcesList.filter(item => {
+      debug(`data source unfiltered list: ${item}`);
+      const result = this._isConnectorOfType(
+        VALID_CONNECTORS_FOR_REPOSITORY,
+        item,
+      );
+      return result;
+    });
+
+    if (availableDatasources.includes(cmdDatasourceName)) {
+      Object.assign(this.artifactInfo, {
+        dataSourceClassName: cmdDatasourceName,
+      });
     }
 
-    if (_.isEmpty(datasourcesList)) {
+    debug(
+      `artifactInfo.dataSourceClassName ${
+        this.artifactInfo.dataSourceClassName
+      }`,
+    );
+
+    if (availableDatasources.length === 0) {
       return this.exit(
-        `${ERROR_NO_DATA_SOURCES_FOUND} ${this.artifactInfo.datasourcesDir}.
+        new Error(
+          `${ERROR_NO_DATA_SOURCES_FOUND} ${this.artifactInfo.datasourcesDir}.
         ${chalk.yellow(
-          'Please visit http://loopback.io/doc/en/lb4/Controller-generator.html for information on how repositories are discovered',
+          'Please visit https://loopback.io/doc/en/lb4/DataSource-generator.html for information on how datasources are discovered',
         )}`,
+        ),
       );
     }
 
@@ -145,48 +266,31 @@ module.exports = class RepositoryGenerator extends ArtifactGenerator {
         type: 'list',
         name: 'dataSourceClassName',
         message: PROMPT_MESSAGE_DATA_SOURCE,
-        choices: datasourcesList,
-        when: this.artifactInfo.dataSourceClassName === undefined,
-        default: datasourcesList[0],
+        choices: availableDatasources,
+        when: !this.artifactInfo.dataSourceClassName,
+        default: availableDatasources[0],
         validate: utils.validateClassName,
       },
     ])
       .then(props => {
-        debug(`props: ${inspect(props)}`);
         Object.assign(this.artifactInfo, props);
+        debug(`props after datasource prompt: ${inspect(props)}`);
         return props;
       })
       .catch(err => {
-        debug(`Error during prompt for datasource name: ${err}`);
+        debug(`Error during datasource prompt: ${err}`);
         return this.exit(err);
       });
   }
 
-  async inferRepositoryType() {
-    let result = await this.isConnectorType(
-      KEY_VALUE_CONNECTOR,
-      this.artifactInfo.dataSourceClassName,
-    );
-
-    if (result) {
-      this.artifactInfo.repositoryTypeClass = KEY_VALUE_REPOSITORY;
-    } else {
-      this.artifactInfo.repositoryTypeClass = DEFAULT_CRUD_REPOSITORY;
-    }
-
-    // assign the data source name to the information artifact
-    let dataSourceName = this.artifactInfo.dataSourceClassName
-      .replace('Datasource', '')
-      .toLowerCase();
-
-    Object.assign(this.artifactInfo, {dataSourceName: dataSourceName});
-    // parent async end() checks for name property, albeit we don't use it here
-    Object.assign(this.artifactInfo, {name: dataSourceName});
-  }
-
   async promptModels() {
+    if (this.shouldExit()) return false;
+
+    await this._inferRepositoryType();
+
     let modelList;
     try {
+      debug(`model list dir ${this.artifactInfo.modelDir}`);
       modelList = await utils.getArtifactList(
         this.artifactInfo.modelDir,
         'model',
@@ -195,72 +299,116 @@ module.exports = class RepositoryGenerator extends ArtifactGenerator {
       return this.exit(err);
     }
 
-    if (_.isEmpty(modelList)) {
+    if (this.options.model) {
+      debug(`Model name received from command line: ${this.options.model}`);
+
+      this.options.model = utils.toClassName(this.options.model);
+      // assign the model name from the command line only if it is valid
+      if (modelList.length > 0 && modelList.includes(this.options.model)) {
+        Object.assign(this.artifactInfo, {modelNameList: [this.options.model]});
+      } else {
+        modelList = [];
+      }
+    }
+
+    if (modelList.length === 0) {
       return this.exit(
-        `${ERROR_NO_MODELS_FOUND} ${this.artifactInfo.modelDir}.
+        new Error(
+          `${ERROR_NO_MODELS_FOUND} ${this.artifactInfo.modelDir}.
         ${chalk.yellow(
-          'Please visit http://loopback.io/doc/en/lb4/Repository-generator.html for information on how models are discovered',
+          'Please visit https://loopback.io/doc/en/lb4/Model-generator.html for information on how models are discovered',
         )}`,
+        ),
       );
     }
 
     return this.prompt([
       {
-        type: 'list',
-        name: 'modelName',
+        type: 'checkbox',
+        name: 'modelNameList',
         message: PROMPT_MESSAGE_MODEL,
         choices: modelList,
-        when: this.artifactInfo.modelName === undefined,
-        default: modelList[0],
-        validate: utils.validateClassName,
-      },
-      {
-        type: 'list',
-        name: 'idType',
-        message: PROMPT_MESSAGE_ID_TYPE,
-        choices: ['number', 'string', 'object'],
-        when: this.artifactInfo.idType === undefined,
-        default: 'number',
+        when: this.artifactInfo.modelNameList === undefined,
       },
     ])
       .then(props => {
-        debug(`props: ${inspect(props)}`);
         Object.assign(this.artifactInfo, props);
+        debug(`props after model list prompt: ${inspect(props)}`);
         return props;
       })
       .catch(err => {
-        debug(`Error during prompt for repository variables: ${err}`);
+        debug(`Error during model list prompt: ${err}`);
         return this.exit(err);
       });
   }
 
-  scaffold() {
-    // We don't want to call the base scaffold function since it copies
-    // all of the templates!
+  async promptModelId() {
     if (this.shouldExit()) return false;
 
-    this.artifactInfo.className = utils.toClassName(this.artifactInfo.name);
+    debug(`Model ID property name from command line: ${this.options.id}`);
+    debug(`Selected Models: ${this.artifactInfo.modelNameList}`);
+
+    if (_.isEmpty(this.artifactInfo.modelNameList)) {
+      return this.exit(new Error(`${ERROR_NO_MODEL_SELECTED}`));
+    } else {
+      // iterate thru each selected model, infer or ask for the ID type
+      for (let item of this.artifactInfo.modelNameList) {
+        this.artifactInfo.modelName = item;
+
+        const prompts = [
+          {
+            type: 'input',
+            name: 'propertyName',
+            message: `Please enter the name of the ID property for ${item}:`,
+            default: 'id',
+          },
+        ];
+
+        let idType = await this._getModelIdType(item);
+
+        /**
+         *  fallback by asking the user for ID property name if the user
+         *  didn't supplied any from the command line. If we inferred it, then
+         *  the supplied argument is ignored
+         */
+        if (idType === null) {
+          if (this.options.id) {
+            idType = this.options.id;
+            /**  make sure it is only used once, in case user selected more
+             * than one model.
+             */
+            delete this.options.id;
+          } else {
+            const answer = await this.prompt(prompts);
+            idType = answer.propertyName;
+          }
+        }
+
+        this.artifactInfo.idType = idType;
+        // Generate this repository
+        await this._scaffold();
+      }
+    }
+  }
+
+  async _scaffold() {
+    if (this.shouldExit()) return false;
+
+    this.artifactInfo.className = utils.toClassName(
+      this.artifactInfo.modelName,
+    );
 
     this.artifactInfo.outFile =
       utils.kebabCase(this.artifactInfo.modelName) + '.repository.ts';
+
     if (debug.enabled) {
       debug(`Artifact output filename set to: ${this.artifactInfo.outFile}`);
     }
 
-    let template = '';
-
-    /* place a switch statement for future repository types */
-    switch (this.artifactInfo.repositoryTypeClass) {
-      case KEY_VALUE_REPOSITORY:
-        template = REPOSITORY_KV_TEMPLATE;
-        break;
-      default:
-        template = REPOSITORY_CRUD_TEMPLATE;
-    }
-
     const source = this.templatePath(
-      path.join('src', 'repositories', template),
+      path.join('src', 'repositories', this.artifactInfo.defaultTemplate),
     );
+
     if (debug.enabled) {
       debug(`Using template at: ${source}`);
     }
@@ -283,6 +431,16 @@ module.exports = class RepositoryGenerator extends ArtifactGenerator {
   }
 
   async end() {
+    this.artifactInfo.type =
+      this.artifactInfo.modelNameList &&
+      this.artifactInfo.modelNameList.length > 1
+        ? 'Repositories'
+        : 'Repository';
+
+    this.artifactInfo.name = this.artifactInfo.modelNameList
+      ? this.artifactInfo.modelNameList.join()
+      : this.artifactInfo.modelName;
+
     await super.end();
   }
 };
