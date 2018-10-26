2e7d4bb6e7b30aedfcc373a0d2bed3c0f1ad07c8
Mario Estrada <marioestradarosa@yahoo.com>
2018-10-26 00:01:51 -0600

    refactor: examples, specifies port,host and OpenAPI server

--- examples/soap-calculator/index.js
+++ examples/soap-calculator/index.js
@@ -4,7 +4,17 @@ module.exports = application;
 
 if (require.main === module) {
   // Run the application
-  application.main().catch(err => {
+  const config = {
+    rest: {
+      port: +process.env.PORT || 3000,
+      host: process.env.HOST || 'localhost',
+      openApiSpec: {
+        // useful when used with OASGraph to locate your application
+        setServersFromRequest: true,
+      },
+    },
+  };
+  application.main(config).catch(err => {
     console.error('Cannot start the application.', err);
     process.exit(1);
   });
