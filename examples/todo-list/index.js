a89bb1eefda94e6c729b775f7e1568ed1253be76
Mario Estrada <marioestradarosa@yahoo.com>
2018-10-26 00:01:51 -0600

    refactor: examples, specifies port,host and OpenAPI server

--- examples/todo-list/index.js
+++ examples/todo-list/index.js
@@ -9,6 +9,16 @@ module.exports = application;
 
 if (require.main === module) {
   // Run the application
+  const config = {
+    rest: {
+      port: +process.env.PORT || 3000,
+      host: process.env.HOST || 'localhost',
+      openApiSpec: {
+        // useful when used with OASGraph to locate your application
+        setServersFromRequest: true,
+      },
+    },
+  };
   application.main().catch(err => {
     console.error('Cannot start the application.', err);
     process.exit(1);
