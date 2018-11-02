ce31bf75a15957f63c15fb5b9ce05f9ea598c751
Raymond Feng <enjoyjava@gmail.com>
2018-11-01 22:18:03 -0700

    fix(rest): improve route sorting to group by path and verb

--- packages/rest/src/router/route-sort.ts
+++ packages/rest/src/router/route-sort.ts
@@ -28,41 +28,30 @@ export function compareRoute(
   route1: Pick<RouteEntry, 'verb' | 'path'>,
   route2: Pick<RouteEntry, 'verb' | 'path'>,
 ): number {
-  // First check verb
-  const verb1 = HTTP_VERBS[route1.verb.toLowerCase()] || HTTP_VERBS.get;
-  const verb2 = HTTP_VERBS[route2.verb.toLowerCase()] || HTTP_VERBS.get;
-  if (verb1 !== verb2) return verb1 - verb2;
-
-  // Then check the path tokens
+  // First check the path tokens
   const path1 = route1.path.replace(/{([^}]*)}(\/|$)/g, ':$1$2');
   const path2 = route2.path.replace(/{([^}]*)}(\/|$)/g, ':$1$2');
   const tokensForPath1: pathToRegExp.Token[] = parse(path1);
   const tokensForPath2: pathToRegExp.Token[] = parse(path2);
 
-  // Longer path comes before shorter path
-  if (tokensForPath1.length < tokensForPath2.length) {
-    return 1;
-  } else if (tokensForPath1.length > tokensForPath2.length) {
-    return -1;
-  }
+  const length =
+    tokensForPath1.length > tokensForPath2.length
+      ? tokensForPath1.length
+      : tokensForPath2.length;
 
-  // Now check token by token
-  for (let i = 0; i < tokensForPath1.length; i++) {
+  for (let i = 0; i < length; i++) {
     const token1 = tokensForPath1[i];
     const token2 = tokensForPath2[i];
-    if (typeof token1 === 'string' && typeof token2 === 'string') {
-      if (token1 < token2) return -1;
-      else if (token1 > token2) return 1;
-    } else if (typeof token1 === 'string' && typeof token2 === 'object') {
-      // token 1 is a literal while token 2 is a parameter
-      return -1;
-    } else if (typeof token1 === 'object' && typeof token2 === 'string') {
-      // token 1 is a parameter while token 2 is a literal
-      return 1;
-    } else {
-      // Both token are parameters. Treat as equal weight.
-    }
+    if (token1 === token2) continue;
+    if (token1 === undefined) return 1;
+    if (token2 === undefined) return -1;
+    if (token1 < token2) return -1;
+    if (token1 > token2) return 1;
   }
+  // Then check verb
+  const verb1 = HTTP_VERBS[route1.verb.toLowerCase()] || HTTP_VERBS.get;
+  const verb2 = HTTP_VERBS[route2.verb.toLowerCase()] || HTTP_VERBS.get;
+  if (verb1 !== verb2) return verb1 - verb2;
   return 0;
 }
 
@@ -77,7 +66,8 @@ function parse(path: string) {
       // The string can be /orders/count
       tokens.push(...p.split('/').filter(Boolean));
     } else {
-      tokens.push(p);
+      // Use `{}` for wildcard as they are larger than any other ascii chars
+      tokens.push(`{}`);
     }
   });
   return tokens;
