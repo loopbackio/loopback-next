0e0425407438bd3b9aff7426d236eb39d371a2bb
YaelGit <y.raz@f5.com>
2019-01-30 13:18:53 +0200

    feat(rest): validate query parameters against their schema
    openapi spec allows developers to provide additional restrictions on input parameters via parameter
    schema. For example, pageSize can include schema restrictions
    
    fix #1573

--- packages/rest/src/parser.ts
+++ packages/rest/src/parser.ts
@@ -11,7 +11,6 @@ import {
   SchemasObject,
 } from '@loopback/openapi-v3-types';
 import * as debugFactory from 'debug';
-//import {RequestBody, RequestBodyParser,RequestQueryParser} from './body-parsers';
 import {RequestBody, RequestBodyParser} from './body-parsers';
 import {coerceParameter} from './coercion/coerce-parameter';
 import {RestHttpErrors} from './rest-http-error';
@@ -32,7 +31,6 @@ export async function parseOperationArgs(
   request: Request,
   route: ResolvedRoute,
   requestBodyParser: RequestBodyParser = new RequestBodyParser(),
-
 ): Promise<OperationArgs> {
   debug('Parsing operation arguments for route %s', route.describe());
   const operationSpec = route.spec;
