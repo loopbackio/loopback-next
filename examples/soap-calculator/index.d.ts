d48eac42cada02f1ede6b886b06b2c7b75932a5b
Diana Lau <dhmlau@ca.ibm.com>
2019-03-11 17:58:36 -0400

    chore: update copyright headers for examples

--- examples/soap-calculator/index.d.ts
+++ examples/soap-calculator/index.d.ts
@@ -2,4 +2,5 @@
 // Node module: @loopback/example-soap-calculator
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
+
 export * from './dist';
