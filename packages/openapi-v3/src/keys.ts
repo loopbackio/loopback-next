83377b574bf3e1189fcf9b9c8dd8ff6465a63b67
Diana Lau <dhmlau@ca.ibm.com>
2019-03-11 17:40:07 -0400

    chore: update copyright header

--- packages/openapi-v3/src/keys.ts
+++ packages/openapi-v3/src/keys.ts
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2018. All Rights Reserved.
+// Node module: @loopback/openapi-v3
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 import {MetadataAccessor} from '@loopback/context';
 import {RestEndpoint, ControllerSpec} from '.';
 import {ParameterObject, RequestBodyObject} from '@loopback/openapi-v3-types';
