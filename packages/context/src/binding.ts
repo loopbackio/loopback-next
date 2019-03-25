e7e8b215211b52623e600ee47dceb307c4776274
Raymond Feng <enjoyjava@gmail.com>
2019-03-25 10:37:37 -0700

    feat(context): pass resolution options into binding.getValue()

--- packages/context/src/binding.ts
+++ packages/context/src/binding.ts
@@ -3,11 +3,16 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-import * as debugModule from 'debug';
+import * as debugFactory from 'debug';
 import {BindingAddress, BindingKey} from './binding-key';
 import {Context} from './context';
 import {Provider} from './provider';
-import {ResolutionSession} from './resolution-session';
+import {
+  asResolutionOptions,
+  ResolutionOptions,
+  ResolutionOptionsOrSession,
+  ResolutionSession,
+} from './resolution-session';
 import {instantiateClass} from './resolver';
 import {
   BoundValue,
@@ -18,7 +23,7 @@ import {
   ValueOrPromise,
 } from './value-promise';
 
-const debug = debugModule('loopback:context:binding');
+const debug = debugFactory('loopback:context:binding');
 
 /**
  * Scope for binding values
@@ -128,6 +133,11 @@ export type BindingTag = TagMap | string;
  */
 export type BindingTemplate<T = unknown> = (binding: Binding<T>) => void;
 
+type ValueGetter<T> = (
+  ctx: Context,
+  options: ResolutionOptions,
+) => ValueOrPromise<T | undefined>;
+
 /**
  * Binding represents an entry in the `Context`. Each binding has a key and a
  * corresponding value getter.
@@ -161,10 +171,7 @@ export class Binding<T = BoundValue> {
   }
 
   private _cache: WeakMap<Context, T>;
-  private _getValue: (
-    ctx?: Context,
-    session?: ResolutionSession,
-  ) => ValueOrPromise<T>;
+  private _getValue: ValueGetter<T>;
 
   private _valueConstructor?: Constructor<T>;
   /**
@@ -228,7 +235,25 @@ export class Binding<T = BoundValue> {
    * @param ctx Context for the resolution
    * @param session Optional session for binding and dependency resolution
    */
-  getValue(ctx: Context, session?: ResolutionSession): ValueOrPromise<T> {
+  getValue(ctx: Context, session?: ResolutionSession): ValueOrPromise<T>;
+
+  /**
+   * Returns a value or promise for this binding in the given context. The
+   * resolved value can be `undefined` if `optional` is set to `true` in
+   * `options`.
+   * @param ctx Context for the resolution
+   * @param options Optional options for binding and dependency resolution
+   */
+  getValue(
+    ctx: Context,
+    options?: ResolutionOptions,
+  ): ValueOrPromise<T | undefined>;
+
+  // Implementation
+  getValue(
+    ctx: Context,
+    optionsOrSession?: ResolutionOptionsOrSession,
+  ): ValueOrPromise<T | undefined> {
     /* istanbul ignore if */
     if (debug.enabled) {
       debug('Get value for binding %s', this.key);
@@ -246,11 +271,15 @@ export class Binding<T = BoundValue> {
         }
       }
     }
+    const options = asResolutionOptions(optionsOrSession);
     if (this._getValue) {
       let result = ResolutionSession.runWithBinding(
-        s => this._getValue(ctx, s),
+        s => {
+          const optionsWithSession = Object.assign({}, options, {session: s});
+          return this._getValue(ctx, optionsWithSession);
+        },
         this,
-        session,
+        options.session,
       );
       return this._cacheValue(ctx, result);
     }
@@ -334,6 +363,14 @@ export class Binding<T = BoundValue> {
   }
 
   /**
+   * Set the `_getValue` function
+   * @param getValue getValue function
+   */
+  private _setValueGetter(getValue: ValueGetter<T>) {
+    this._getValue = getValue;
+  }
+
+  /**
    * Bind the key to a constant value. The value must be already available
    * at binding time, it is not allowed to pass a Promise instance.
    *
@@ -372,7 +409,7 @@ export class Binding<T = BoundValue> {
       debug('Bind %s to constant:', this.key, value);
     }
     this._type = BindingType.CONSTANT;
-    this._getValue = () => value;
+    this._setValueGetter(() => value);
     return this;
   }
 
@@ -400,7 +437,7 @@ export class Binding<T = BoundValue> {
       debug('Bind %s to dynamic value:', this.key, factoryFn);
     }
     this._type = BindingType.DYNAMIC_VALUE;
-    this._getValue = ctx => factoryFn();
+    this._setValueGetter(ctx => factoryFn());
     return this;
   }
 
@@ -426,14 +463,14 @@ export class Binding<T = BoundValue> {
       debug('Bind %s to provider %s', this.key, providerClass.name);
     }
     this._type = BindingType.PROVIDER;
-    this._getValue = (ctx, session) => {
+    this._setValueGetter((ctx, options) => {
       const providerOrPromise = instantiateClass<Provider<T>>(
         providerClass,
-        ctx!,
-        session,
+        ctx,
+        options.session,
       );
       return transformValueOrPromise(providerOrPromise, p => p.value());
-    };
+    });
     return this;
   }
 
@@ -450,11 +487,16 @@ export class Binding<T = BoundValue> {
       debug('Bind %s to class %s', this.key, ctor.name);
     }
     this._type = BindingType.CLASS;
-    this._getValue = (ctx, session) => instantiateClass(ctor, ctx!, session);
+    this._setValueGetter((ctx, options) =>
+      instantiateClass(ctor, ctx, options.session),
+    );
     this._valueConstructor = ctor;
     return this;
   }
 
+  /**
+   * Unlock the binding
+   */
   unlock(): this {
     this.isLocked = false;
     return this;
