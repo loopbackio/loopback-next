641cbd01cd4126f63a1bb9ecef85781c034c56dc
Pradeep Kumar Tippa <pktippa@gmail.com>
2019-04-03 21:41:37 +0530

    fix(rest): a small typo fix in code comments

--- packages/rest/src/validation/request-body.validator.ts
+++ packages/rest/src/validation/request-body.validator.ts
@@ -24,7 +24,7 @@ export type RequestBodyValidationOptions = AJV.Options;
  * Check whether the request body is valid according to the provided OpenAPI schema.
  * The JSON schema is generated from the OpenAPI schema which is typically defined
  * by `@requestBody()`.
- * The validation leverages AJS schema validator.
+ * The validation leverages AJV schema validator.
  * @param body The request body parsed from an HTTP request.
  * @param requestBodySpec The OpenAPI requestBody specification defined in `@requestBody()`.
  * @param globalSchemas The referenced schemas generated from `OpenAPISpec.components.schemas`.
