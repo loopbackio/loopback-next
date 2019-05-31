c7e14caca50e2827a0ee3ebfcdc96b9e5a0ccd74
Miroslav Bajtoš <mbajtoss@gmail.com>
2019-05-31 12:45:39 +0200

    feat(testlab): rework itSkippedOnTravis to support Jest framework
    Describe referenced Mocha types directly in testlab to avoid
    compile-time dependency on Mocha types, some of which are not
    compatible with Jest.
    
    Signed-off-by: Miroslav Bajtoš <mbajtoss@gmail.com>

--- packages/testlab/src/skip-travis.ts
+++ packages/testlab/src/skip-travis.ts
@@ -3,8 +3,21 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-// tslint:disable-next-line:no-any
-export type TestCallbackRetval = void | PromiseLike<any>;
+// Simplified test function type from Mocha
+export interface TestFn {
+  (this: TestContext): PromiseLike<unknown>;
+  (this: TestContext, done: Function): void;
+}
+
+// Type of "this" object provided by Mocha to test functions
+export interface TestContext {
+  skip(): this;
+  timeout(ms: number | string): this;
+  retries(n: number): this;
+  slow(ms: number): this;
+  // tslint:disable-next-line:no-any
+  [index: string]: any;
+}
 
 /**
  * Helper function for skipping tests on Travis env
@@ -13,10 +26,7 @@ export type TestCallbackRetval = void | PromiseLike<any>;
  */
 export function itSkippedOnTravis(
   expectation: string,
-  callback?: (
-    this: Mocha.ITestCallbackContext,
-    done: MochaDone,
-  ) => TestCallbackRetval,
+  callback?: TestFn,
 ): void {
   if (process.env.TRAVIS) {
     it.skip(`[SKIPPED ON TRAVIS] ${expectation}`, callback);
