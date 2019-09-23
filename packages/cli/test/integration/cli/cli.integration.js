a6863e5c2c912927fc2c1c70b595a0f0cc0c9fe9
Miroslav Bajtoš <mbajtoss@gmail.com>
2019-09-23 09:07:25 +0200

    test(cli): use snapshots to match list of commands in help
    Signed-off-by: Miroslav Bajtoš <mbajtoss@gmail.com>

--- packages/cli/test/integration/cli/cli.integration.js
+++ packages/cli/test/integration/cli/cli.integration.js
@@ -8,6 +8,7 @@
 const expect = require('@loopback/testlab').expect;
 const util = require('util');
 const main = require('../../../lib/cli');
+const {expectToMatchSnapshot} = require('../../snapshots');
 
 function getLog(buffer) {
   buffer = buffer || [];
@@ -21,13 +22,7 @@ describe('cli', () => {
   it('lists available commands', () => {
     const entries = [];
     main({commands: true}, getLog(entries));
-    expect(entries).to.eql([
-      'Available commands: ',
-      '  lb4 app\n  lb4 extension\n  lb4 controller\n  lb4 datasource\n  ' +
-        'lb4 model\n  lb4 repository\n  lb4 service\n  lb4 example\n  ' +
-        'lb4 openapi\n  lb4 observer\n  lb4 interceptor\n  lb4 discover\n  ' +
-        'lb4 relation',
-    ]);
+    expectToMatchSnapshot(entries.join('\n'));
   });
 
   it('lists versions', () => {
@@ -41,13 +36,7 @@ describe('cli', () => {
   it('prints commands with --help', () => {
     const entries = [];
     main({help: true, _: []}, getLog(entries), true);
-    expect(entries).to.containEql('Available commands: ');
-    expect(entries).to.containEql(
-      '  lb4 app\n  lb4 extension\n  lb4 controller\n  lb4 datasource\n  ' +
-        'lb4 model\n  lb4 repository\n  lb4 service\n  lb4 example\n  ' +
-        'lb4 openapi\n  lb4 observer\n  lb4 interceptor\n  lb4 discover\n  ' +
-        'lb4 relation',
-    );
+    expectToMatchSnapshot(entries.join('\n'));
   });
 
   it('does not print commands with --help for a given command', () => {
