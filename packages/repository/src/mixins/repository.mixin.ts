9bd5cf0035d36cab5fb2962dd4e53cb3ac0bdf6a
Raymond Feng <enjoyjava@gmail.com>
2019-10-02 09:29:01 -0700

    chore: remove dummy constructor for mixins

--- packages/repository/src/mixins/repository.mixin.ts
+++ packages/repository/src/mixins/repository.mixin.ts
@@ -29,12 +29,6 @@ const debug = debugFactory('loopback:repository:mixin');
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function RepositoryMixin<T extends Class<any>>(superClass: T) {
   return class extends superClass {
-    // A mixin class has to take in a type any[] argument!
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    constructor(...args: any[]) {
-      super(...args);
-    }
-
     /**
      * Add a repository to this application.
      *
