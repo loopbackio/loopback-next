e8d8034c107e08ecb8404a39805dd4e81d9c2b6a
Miroslav Bajtoš <mbajtoss@gmail.com>
2019-11-18 10:09:05 +0100

    test(cli): use snaphots in model discovery tests
    Signed-off-by: Miroslav Bajtoš <mbajtoss@gmail.com>

---
+++ packages/cli/snapshots/integration/generators/discover.integration.snapshots.js
@@ -0,0 +1,114 @@
+// IMPORTANT
+// This snapshot file is auto-generated, but designed for humans.
+// It should be checked into source control and tracked carefully.
+// Re-generate by setting UPDATE_SNAPSHOTS=1 and running tests.
+// Make sure to inspect the changes in the snapshots below.
+// Do not ignore changes!
+
+'use strict';
+
+exports[`lb4 discover integration model discovery generates all models without prompts using --all --dataSource 1`] = `
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Schema extends Entity {
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Schema>) {
+    super(data);
+  }
+}
+
+export interface SchemaRelations {
+  // describe navigational properties here
+}
+
+export type SchemaWithRelations = Schema & SchemaRelations;
+
+`;
+
+
+exports[`lb4 discover integration model discovery generates all models without prompts using --all --dataSource 2`] = `
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class View extends Entity {
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<View>) {
+    super(data);
+  }
+}
+
+export interface ViewRelations {
+  // describe navigational properties here
+}
+
+export type ViewWithRelations = View & ViewRelations;
+
+`;
+
+
+exports[`lb4 discover integration model discovery uses a different --outDir if provided 1`] = `
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Test extends Entity {
+  @property({
+    type: Date,
+    required: false,
+  })
+  dateTest?: Date;
+
+  @property({
+    type: Number,
+    required: false,
+  })
+  numberTest?: Number;
+
+  @property({
+    type: String,
+    required: false,
+  })
+  stringTest?: String;
+
+  @property({
+    type: Boolean,
+    required: false,
+  })
+  booleanText?: Boolean;
+
+  @property({
+    type: Number,
+    required: true,
+    scale: 0,
+    id: 1,
+  })
+  id: Number;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Test>) {
+    super(data);
+  }
+}
+
+export interface TestRelations {
+  // describe navigational properties here
+}
+
+export type TestWithRelations = Test & TestRelations;
+
+`;
