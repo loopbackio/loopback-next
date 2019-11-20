ad4420954e3d3c18d4a52ca7511985d026efcdc6
Raymond Feng <enjoyjava@gmail.com>
2019-11-19 23:09:32 -0800

    feat(rest): upgrade to path-to-regexp 6.x and improve error messages

--- packages/rest/src/router/openapi-path.ts
+++ packages/rest/src/router/openapi-path.ts
@@ -13,42 +13,37 @@ import {parse} from 'path-to-regexp';
  * allows `[A-Za-z0-9_]`
  */
 const POSSIBLE_VARNAME_PATTERN = /\{([^\}]+)\}/g;
-const INVALID_VARNAME_PATTERN = /\{([^\}]*[^\w\}][^\}]*)\}/;
+const VALID_VARNAME_PATTERN = /^[A-Za-z0-9_]+$/;
 
 /**
  * Validate the path to be compatible with OpenAPI path template. No parameter
  * modifier, custom pattern, or unnamed parameter is allowed.
  */
 export function validateApiPath(path = '/') {
-  let tokens = parse(path);
-  if (tokens.some(t => typeof t === 'object')) {
-    throw new Error(
-      `Invalid path template: '${path}'. Please use {param} instead of ':param'`,
-    );
-  }
-
-  const invalid = path.match(INVALID_VARNAME_PATTERN);
-  if (invalid) {
-    throw new Error(
-      `Invalid parameter name '${invalid[1]}' found in path '${path}'`,
-    );
-  }
-
-  const regexpPath = toExpressPath(path);
-  tokens = parse(regexpPath);
+  const tokens = parse(path);
   for (const token of tokens) {
     if (typeof token === 'string') continue;
-    if (typeof token.name === 'number') {
-      // Such as /(.*)
-      throw new Error(`Unnamed parameter is not allowed in path '${path}'`);
-    }
-    if (
-      (token.optional || token.repeat || token.pattern !== '[^\\/]+?') &&
-      // Required by path-to-regexp@3.x
-      token.prefix === '/'
-    ) {
-      // Such as /:foo*, /:foo+, /:foo?, or /:foo(\\d+)
-      throw new Error(`Parameter modifier is not allowed in path '${path}'`);
+    if (typeof token === 'object') {
+      const name = token.name;
+      if (typeof name === 'string' && name !== '') {
+        throw new Error(
+          `Invalid path template: '${path}'. Please use {${name}} instead of ':${name}'`,
+        );
+      }
+      if (typeof name === 'number') {
+        throw new Error(`Unnamed parameter is not allowed in path '${path}'`);
+      }
+      const valid = token.prefix.match(VALID_VARNAME_PATTERN);
+      if (!valid) {
+        throw new Error(
+          `Invalid parameter name '${token.prefix}' found in path '${path}'`,
+        );
+      }
+      if (['?', '+', '*'].includes(token.modifier)) {
+        throw new Error(
+          `Parameter modifier '{${token.prefix}}${token.modifier}' is not allowed in path '${path}`,
+        );
+      }
     }
   }
   return path;
@@ -68,5 +63,5 @@ export function getPathVariables(path: string) {
 export function toExpressPath(path: string) {
   // Convert `.` to `\\.` so that path-to-regexp will treat it as the plain
   // `.` character
-  return path.replace(POSSIBLE_VARNAME_PATTERN, ':$1').replace('.', '\\.');
+  return path.replace(POSSIBLE_VARNAME_PATTERN, '{:$1}').replace('.', '\\.');
 }
