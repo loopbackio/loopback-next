b3b3f3f075740092d64f9411e9b6a7893916bf06
Raymond Feng <enjoyjava@gmail.com>
2019-11-19 16:21:10 -0800

    feat(extension-logging): add http access and method invocation logging
    - add architecture diagram and examples to README
    - improve naming and exporting
    - configure the http access logger for REST routes only

--- extensions/logging/src/__tests__/accpetance/logging.component.acceptance.ts
+++ extensions/logging/src/__tests__/accpetance/logging.component.acceptance.ts
@@ -45,3 +45,42 @@ describe('LoggingComponent', () => {
     return new Application();
   }
 });
+
+describe('LoggingComponent without fluent', () => {
+  let app: Application;
+
+  before(givenAppAndLoggingComponent);
+
+  it('does not bind a fluent sender', async () => {
+    expect(app.isBound(LoggingBindings.FLUENT_SENDER)).to.be.false();
+  });
+
+  it('binds a winston logger', async () => {
+    expect(app.isBound(LoggingBindings.WINSTON_LOGGER)).to.be.true();
+    const logger = await app.get(LoggingBindings.WINSTON_LOGGER);
+    expect(logger.log).to.be.Function();
+  });
+
+  it('does not bind a winston transport for fluent', async () => {
+    expect(app.isBound(LoggingBindings.WINSTON_TRANSPORT_FLUENT)).to.be.false();
+  });
+
+  it('does not bind a winston access log', async () => {
+    expect(
+      app.isBound(LoggingBindings.WINSTON_HTTP_ACCESS_LOGGER),
+    ).to.be.false();
+  });
+
+  async function givenAppAndLoggingComponent() {
+    app = givenApplication();
+    app.configure(LoggingBindings.COMPONENT).to({
+      enableFluent: false,
+      enableHttpAccessLog: false,
+    });
+    app.component(LoggingComponent);
+  }
+
+  function givenApplication() {
+    return new Application();
+  }
+});
