f45ce03579db6f7524c4cd4d71b13bc1464062de
Agnes Lin <agneslin.lin@ibm.com>
2019-12-20 15:48:03 -0500

    fix(repository): belongsto accessor should return undefined if foreign key is not included

--- packages/repository-tests/src/crud/relations/acceptance/belongs-to.relation.acceptance.ts
+++ packages/repository-tests/src/crud/relations/acceptance/belongs-to.relation.acceptance.ts
@@ -93,6 +93,18 @@ export function belongsToRelationAcceptance(
       expect(result).to.deepEqual(shipment);
     });
 
+    it('returns undefined if the source instance does not have the foreign key', async () => {
+      await shipmentRepo.create({
+        name: 'Tuesday morning shipment',
+      });
+      const order = await orderRepo.create({
+        // doesn't have the foreign key
+        description: 'Order that is shipped Tuesday morning',
+      });
+      const result = await orderRepo.shipment(order.id);
+      expect(result).to.be.undefined();
+    });
+
     it('throws EntityNotFound error when the related model does not exist', async () => {
       const deletedCustomer = await customerRepo.create({
         name: 'Order McForder',
