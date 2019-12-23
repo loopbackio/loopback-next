6105883967ca5853cc8990f423d9febd1eb07101
dougal83 <dougal83+git@gmail.com>
2019-12-23 18:06:42 +0000

    feat(repository-json-schema): add title to filter schemas
    add title property to filter schemas(filter, where, scope) in preparation for openapi schema consolidation
    
    Signed-off-by: Douglas McConnachie <dougal83+git@gmail.com>

--- packages/openapi-v3/src/__tests__/unit/filter-schema.unit.ts
+++ packages/openapi-v3/src/__tests__/unit/filter-schema.unit.ts
@@ -21,13 +21,54 @@ describe('filterSchema', () => {
     const schema = getFilterSchemaFor(MyUserModel);
     expect(MyUserModel.definition.name).to.eql('my-user-model');
     expect(schema).to.eql({
+      title: 'my-user-model.Filter',
       properties: {
         where: {
           type: 'object',
+          title: 'my-user-model.WhereFilter',
           additionalProperties: true,
         },
         fields: {
           type: 'object',
+          title: 'my-user-model.Fields',
+          properties: {
+            id: {type: 'boolean'},
+            age: {type: 'boolean'},
+          },
+          additionalProperties: false,
+        },
+        offset: {type: 'integer', minimum: 0},
+        limit: {type: 'integer', minimum: 1, example: 100},
+        skip: {type: 'integer', minimum: 0},
+        order: {type: 'array', items: {type: 'string'}},
+      },
+      additionalProperties: false,
+    });
+  });
+
+  @model({
+    name: 'CustomUserModel',
+  })
+  class CustomUserModel extends Entity {
+    @property() id: string;
+
+    @property() age: number;
+  }
+
+  it('generates filter schema with custom name', () => {
+    const schema = getFilterSchemaFor(CustomUserModel);
+    expect(CustomUserModel.definition.name).to.eql('CustomUserModel');
+    expect(schema).to.eql({
+      title: 'CustomUserModel.Filter',
+      properties: {
+        where: {
+          type: 'object',
+          title: 'CustomUserModel.WhereFilter',
+          additionalProperties: true,
+        },
+        fields: {
+          type: 'object',
+          title: 'CustomUserModel.Fields',
           properties: {
             id: {type: 'boolean'},
             age: {type: 'boolean'},
