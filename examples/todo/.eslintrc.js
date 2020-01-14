48528843d4b2dbc075ffc04c97b7feb1769f38a1
jannyHou <juehou@ca.ibm.com>
2020-01-14 13:10:09 -0500

    chore: ignore js file

---
+++ examples/todo/.eslintrc.js
@@ -0,0 +1,8 @@
+// Copyright IBM Corp. 2018. All Rights Reserved.
+// Node module: @loopback/example-todo
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+module.exports = {
+  extends: ['@loopback/eslint-config'],
+};
