297e423d88a9ae19060c95f35c278b3414f7e0c1
Raymond Feng <enjoyjava@gmail.com>
2020-02-04 15:42:51 -0800

    chore(core): use tag filter for life cycle observers
    - This can leverage tag index for bindings to match life cycle observers

--- packages/core/src/lifecycle.ts
+++ packages/core/src/lifecycle.ts
@@ -8,6 +8,7 @@ import {
   Binding,
   BindingSpec,
   Constructor,
+  filterByTag,
   ValueOrPromise,
 } from '@loopback/context';
 import {CoreTags} from './keys';
@@ -61,9 +62,9 @@ export function asLifeCycleObserver<T = unknown>(binding: Binding<T>) {
  * Find all life cycle observer bindings. By default, a binding tagged with
  * `CoreTags.LIFE_CYCLE_OBSERVER`. It's used as `BindingFilter`.
  */
-export function lifeCycleObserverFilter(binding: Readonly<Binding>): boolean {
-  return binding.tagMap[CoreTags.LIFE_CYCLE_OBSERVER] != null;
-}
+export const lifeCycleObserverFilter = filterByTag(
+  CoreTags.LIFE_CYCLE_OBSERVER,
+);
 
 /**
  * Sugar decorator to mark a class as life cycle observer
