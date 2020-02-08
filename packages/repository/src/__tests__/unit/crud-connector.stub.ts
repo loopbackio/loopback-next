b7b3f88a9a12b01d14a1df0d3efa09b59e06e1d8
Diana Lau <dhmlau@ca.ibm.com>
2020-02-08 16:45:59 -0500

    chore: update copyrights year

--- packages/repository/src/__tests__/unit/crud-connector.stub.ts
+++ packages/repository/src/__tests__/unit/crud-connector.stub.ts
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2019. All Rights Reserved.
+// Node module: @loopback/repository
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
 import {Callback} from 'loopback-datasource-juggler';
 import {
   AnyObject,
