a1431a99721d81a66d533692fd417cc0e20b16cc
Miroslav Bajtoš <mbajtoss@gmail.com>
2020-02-11 10:38:34 +0100

    feat: use spec-based routing
    Signed-off-by: Miroslav Bajtoš <mbajtoss@gmail.com>

--- examples/todo/README.md
+++ examples/todo/README.md
@@ -12,3 +12,25 @@ $  curl http://127.0.0.1:3000/openapi.json > api.json
 
 The next step is to configure the application to use the OpenAPI document.
 (Note that at this point, the app won't start because of duplicate operations.)
+
+Now we can remove OpenAPI decorators from controller code.
+
+Please note that the OpenAPI spec document already contains extension fields
+mapping operations to controller methods implementing them, because the spec
+was produced by the LoopBack app. In a real world, the person creating the spec
+and/or the application developer will have to set extension fields
+`x-controller-name` and `x-operation-name`.
+
+Idea: maybe we should have a decorator to map controller methods to operations,
+so that we don't need to touch the spec. For example:
+
+```ts
+class TodoController {
+  // ...
+
+  @implements('TodoController.replaceTodo')
+  async replaceTodo(id: number, todo: Todo): Promise<void> {
+    await this.todoRepository.replaceById(id, todo);
+  }
+}
+```
--- examples/todo/src/controllers/todo.controller.ts
+++ examples/todo/src/controllers/todo.controller.ts
@@ -5,17 +5,6 @@
 
 import {inject} from '@loopback/core';
 import {Filter, repository} from '@loopback/repository';
-import {
-  del,
-  get,
-  getFilterSchemaFor,
-  getModelSchemaRef,
-  param,
-  patch,
-  post,
-  put,
-  requestBody,
-} from '@loopback/rest';
 import {Todo} from '../models';
 import {TodoRepository} from '../repositories';
 import {Geocoder} from '../services';
@@ -26,24 +15,7 @@ export class TodoController {
     @inject('services.Geocoder') protected geoService: Geocoder,
   ) {}
 
-  @post('/todos', {
-    responses: {
-      '200': {
-        description: 'Todo model instance',
-        content: {'application/json': {schema: getModelSchemaRef(Todo)}},
-      },
-    },
-  })
-  async createTodo(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Todo, {title: 'NewTodo', exclude: ['id']}),
-        },
-      },
-    })
-    todo: Omit<Todo, 'id'>,
-  ): Promise<Todo> {
+  async createTodo(todo: Omit<Todo, 'id'>): Promise<Todo> {
     if (todo.remindAtAddress) {
       // TODO(bajtos) handle "address not found"
       const geo = await this.geoService.geocode(todo.remindAtAddress);
@@ -55,83 +27,23 @@ export class TodoController {
     return this.todoRepository.create(todo);
   }
 
-  @get('/todos/{id}', {
-    responses: {
-      '200': {
-        description: 'Todo model instance',
-        content: {'application/json': {schema: getModelSchemaRef(Todo)}},
-      },
-    },
-  })
-  async findTodoById(
-    @param.path.number('id') id: number,
-    @param.query.boolean('items') items?: boolean,
-  ): Promise<Todo> {
+  async findTodoById(id: number, items?: boolean): Promise<Todo> {
     return this.todoRepository.findById(id);
   }
 
-  @get('/todos', {
-    responses: {
-      '200': {
-        description: 'Array of Todo model instances',
-        content: {
-          'application/json': {
-            schema: {type: 'array', items: getModelSchemaRef(Todo)},
-          },
-        },
-      },
-    },
-  })
-  async findTodos(
-    @param.query.object('filter', getFilterSchemaFor(Todo))
-    filter?: Filter<Todo>,
-  ): Promise<Todo[]> {
+  async findTodos(filter?: Filter<Todo>): Promise<Todo[]> {
     return this.todoRepository.find(filter);
   }
 
-  @put('/todos/{id}', {
-    responses: {
-      '204': {
-        description: 'Todo PUT success',
-      },
-    },
-  })
-  async replaceTodo(
-    @param.path.number('id') id: number,
-    @requestBody() todo: Todo,
-  ): Promise<void> {
+  async replaceTodo(id: number, todo: Todo): Promise<void> {
     await this.todoRepository.replaceById(id, todo);
   }
 
-  @patch('/todos/{id}', {
-    responses: {
-      '204': {
-        description: 'Todo PATCH success',
-      },
-    },
-  })
-  async updateTodo(
-    @param.path.number('id') id: number,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Todo, {partial: true}),
-        },
-      },
-    })
-    todo: Partial<Todo>,
-  ): Promise<void> {
+  async updateTodo(id: number, todo: Partial<Todo>): Promise<void> {
     await this.todoRepository.updateById(id, todo);
   }
 
-  @del('/todos/{id}', {
-    responses: {
-      '204': {
-        description: 'Todo DELETE success',
-      },
-    },
-  })
-  async deleteTodo(@param.path.number('id') id: number): Promise<void> {
+  async deleteTodo(id: number): Promise<void> {
     await this.todoRepository.deleteById(id);
   }
 }
