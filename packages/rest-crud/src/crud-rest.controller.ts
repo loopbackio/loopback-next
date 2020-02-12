a841855e89ef89485c80bceb8ab6ea3d438d1201
Raymond Feng <enjoyjava@gmail.com>
2020-02-12 08:23:46 -0800

    chore: fix tsdoc comments so that apidocs can be rendered correctly
    Fixes https://github.com/strongloop/loopback-next/issues/4631

--- packages/rest-crud/src/crud-rest.controller.ts
+++ packages/rest-crud/src/crud-rest.controller.ts
@@ -105,7 +105,7 @@ export interface CrudRestControllerOptions {
 /**
  * Create (define) a CRUD Controller class for the given model.
  *
- * Example usage:
+ * @example
  *
  * ```ts
  * const ProductController = defineCrudRestController<
