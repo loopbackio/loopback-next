01a16d3b1305443548acf41fde1f458076ca2e60
Raymond Feng <enjoyjava@gmail.com>
2020-03-11 13:14:19 -0700

    feat(example-file-upload-download): validate file name to avoid access beyond the destination

--- examples/file-upload-download/src/__tests__/acceptance/file-download.acceptance.ts
+++ examples/file-upload-download/src/__tests__/acceptance/file-download.acceptance.ts
@@ -32,6 +32,28 @@ describe('file download acceptance', () => {
     await client.get('/file-download/does-not-exst.txt').expect(404);
   });
 
+  it('reports 400 if file name is resolved outside the sandbox', async () => {
+    const badFileName = '%2Fbad-file.txt'; // `/bad-file.txt`
+    await client.get(`/file-download/${badFileName}`).expect(400, {
+      error: {
+        statusCode: 400,
+        name: 'BadRequestError',
+        message: 'Invalid file name: /bad-file.txt',
+      },
+    });
+  });
+
+  it('reports 400 if file name is resolved outside the sandbox with ..', async () => {
+    const badFileName = '%2E%2E%2Fbad-file.txt'; // `../bad-file.txt`
+    await client.get(`/file-download/${badFileName}`).expect(400, {
+      error: {
+        statusCode: 400,
+        name: 'BadRequestError',
+        message: 'Invalid file name: ../bad-file.txt',
+      },
+    });
+  });
+
   it('download a file', async () => {
     await sandbox.writeJsonFile('test.json', {test: 'JSON'});
     await client.get('/file-download/test.json').expect(200, {test: 'JSON'});
