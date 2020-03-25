d2a49a0bfcec8f1e90fc3bba620f5439d053f38b
Diana Lau <dhmlau@ca.ibm.com>
2020-03-25 13:56:14 -0400

    feat(example-validation-app): customize error

--- examples/validation-app/src/__tests__/acceptance/validate.acceptance.ts
+++ examples/validation-app/src/__tests__/acceptance/validate.acceptance.ts
@@ -3,7 +3,7 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-import {Client} from '@loopback/testlab';
+import {Client, expect} from '@loopback/testlab';
 import {ValidationApplication} from '../..';
 import {setupApplication} from './test-helper';
 
@@ -31,30 +31,49 @@ describe('validate properties', () => {
     await client.post('/coffee-shops').send(validCoffeeShop).expect(200);
   });
 
-  it('create CoffeeShop failed with city name exceeds length limit', async () => {
+  it('fails when creating a CoffeeShop with the city name exceeds length limit', async () => {
     const invalidCityNameCS = validCoffeeShop;
     invalidCityNameCS.city = 'Tooooooooooronto';
     await client.post('/coffee-shops').send(invalidCityNameCS).expect(422);
   });
 
-  it('create CoffeeShop failed with invalid phone number', async () => {
+  it('fails when creating a CoffeeShop with an invalid phone number', async () => {
     const invalidPhoneNumCS = validCoffeeShop;
     invalidPhoneNumCS.phoneNum = '1111111111';
     await client.post('/coffee-shops').send(invalidPhoneNumCS).expect(422);
   });
 
-  it('create CoffeeShop failed with capacity exceeds limit', async () => {
+  it('fails when creating a CoffeeShop with the capacity exceeds limit', async () => {
     const invalidCapacityCS = validCoffeeShop;
     invalidCapacityCS.capacity = 10000;
     await client.post('/coffee-shops').send(invalidCapacityCS).expect(422);
   });
 
-  it('create CoffeeShop failed with invalid area code', async () => {
+  it('fails when creating a CoffeeShop with an invalid area code', async () => {
     const invalidAreaCodeCS = {
       city: 'Toronto',
       phoneNum: '999-111-1111',
       capacity: 10,
     };
-    await client.post('/coffee-shops').send(invalidAreaCodeCS).expect(422);
+    const response = await client.post('/coffee-shops').send(invalidAreaCodeCS);
+    expect(response.status).to.eql(400);
+    expect(response.body.error.message).to.eql(
+      'Area code and city do not match',
+    );
+  });
+
+  it('fails when creating a CoffeeShop with an invalid type', async () => {
+    const invalidCapacityTypeCS = {
+      city: 'Toronto',
+      phoneNum: '416-111-1111',
+      capacity: '10',
+    };
+    const response = await client
+      .post('/coffee-shops')
+      .send(invalidCapacityTypeCS)
+      .expect(422);
+
+    // with customized response
+    expect(response.body).to.have.property('resolution');
   });
 });
