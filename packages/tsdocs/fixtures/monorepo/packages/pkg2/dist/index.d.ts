0ebd44d6e1c8dd0fba380e95b8f67e78e9653327
Raymond Feng <enjoyjava@gmail.com>
2020-04-08 09:11:49 -0700

    chore(tsdocs): remove copyright headers from files that can be regenerated

--- packages/tsdocs/fixtures/monorepo/packages/pkg2/dist/index.d.ts
+++ packages/tsdocs/fixtures/monorepo/packages/pkg2/dist/index.d.ts
@@ -1,8 +1,3 @@
-// Copyright IBM Corp. 2019. All Rights Reserved.
-// Node module: @loopback/tsdocs
-// This file is licensed under the MIT License.
-// License text available at https://opensource.org/licenses/MIT
-
 import { Pet } from 'pkg1';
 /**
  * Dog
