9b96f0a44cc1e6a1aadfb1e2293d94eb18b759da
Raymond Feng <enjoyjava@gmail.com>
2020-04-10 09:06:12 -0700

    feat(example-multi-tenancy): scaffold an example to demostrate multi-tenancy

---
+++ examples/multi-tenancy/README.md
@@ -0,0 +1,6 @@
+# @loopback/example-multi-tenancy
+
+An example application to demonstrate how to implement multi-tenancy with
+LoopBack 4.
+
+[![LoopBack](<https://github.com/strongloop/loopback-next/raw/master/docs/site/imgs/branding/Powered-by-LoopBack-Badge-(blue)-@2x.png>)](http://loopback.io/)
---
+++ examples/multi-tenancy/src/__tests__/acceptance/test-helper.ts
@@ -0,0 +1,37 @@
+// Copyright IBM Corp. 2020. All Rights Reserved.
+// Node module: @loopback/example-multi-tenancy
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {ExampleMultiTenancyApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const restConfig = givenHttpServerConfig({
+    // Customize the server configuration here.
+    // Empty values (undefined, '') will be ignored by the helper.
+    //
+    // host: process.env.HOST,
+    // port: +process.env.PORT,
+  });
+
+  const app = new ExampleMultiTenancyApplication({
+    rest: restConfig,
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: ExampleMultiTenancyApplication;
+  client: Client;
+}
