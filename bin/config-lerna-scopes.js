337058c69bec77b7378bfabab02139d5a034e603
Raymond Feng <enjoyjava@gmail.com>
2020-04-24 07:52:39 -0700

    chore: allow to print out valid conventional commit scopes

--- bin/config-lerna-scopes.js
+++ bin/config-lerna-scopes.js
@@ -9,15 +9,16 @@
  */
 'use strict';
 
-const getPackages = require('@lerna/project').getPackages;
+const {getPackages} = require('@lerna/project');
+
 module.exports = {
   rules: {
     // https://github.com/marionebl/commitlint/blob/master/docs/reference-rules.md
-    'scope-enum': async ctx => [2, 'always', await listPackages(ctx)],
+    'scope-enum': async ctx => [2, 'always', await getPackageNames(ctx)],
   },
 };
 
-async function listPackages(context) {
+async function getPackageNames(context) {
   const ctx = context || {};
   const cwd = ctx.cwd || process.cwd();
   // List all lerna packages
@@ -25,7 +26,25 @@ async function listPackages(context) {
 
   // Get a list of names. Npm scopes will be removed, for example,
   // @loopback/core -> core
-  return packages
-    .map(pkg => pkg.name)
-    .map(name => (name.charAt(0) === '@' ? name.split('/')[1] : name));
+  return packages.map(getShortName);
+}
+
+/**
+ * Get short name of a package. The NPM scope will be removed if it exists.
+ * For example, the short name of `@loopback/core` is `core`.
+ * @param {Package} pkg - Lerna project
+ */
+function getShortName(pkg) {
+  const name = pkg.name;
+  return name.startsWith('@') ? name.split('/')[1] : name;
+}
+
+if (require.main === module) {
+  getPackageNames().then(
+    names => console.log('Scopes: %s', names),
+    err => {
+      console.error(err);
+      process.exit(1);
+    },
+  );
 }
