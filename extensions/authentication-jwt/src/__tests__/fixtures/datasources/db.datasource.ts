0973d18ee4e94391367ee3510a9f0bc2eebaf4b7
Raymond Feng <enjoyjava@gmail.com>
2020-04-24 22:35:23 -0700

    fix(authentication-jwt): fix package.json and rename the package

--- extensions/authentication-jwt/src/__tests__/fixtures/datasources/db.datasource.ts
+++ extensions/authentication-jwt/src/__tests__/fixtures/datasources/db.datasource.ts
@@ -1,5 +1,5 @@
-// Copyright IBM Corp. 2018,2019. All Rights Reserved.
-// Node module: @loopback/example-todo
+// Copyright IBM Corp. 2020. All Rights Reserved.
+// Node module: @loopback/authentication-jwt
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
