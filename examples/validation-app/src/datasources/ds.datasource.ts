df4ee09482fa67522629c381a0de595ce12d9a1b
Miroslav Bajtoš <mbajtoss@gmail.com>
2020-04-27 14:25:44 +0200

    feat: remove hand-written datasource `stop()` method
    Use the `stop()` method provided by `juggler.DataSource` instead.
    
    Signed-off-by: Miroslav Bajtoš <mbajtoss@gmail.com>

--- examples/validation-app/src/datasources/ds.datasource.ts
+++ examples/validation-app/src/datasources/ds.datasource.ts
@@ -3,12 +3,7 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-import {
-  inject,
-  lifeCycleObserver,
-  LifeCycleObserver,
-  ValueOrPromise,
-} from '@loopback/core';
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 
 const config = {
@@ -29,20 +24,5 @@ export class DsDataSource extends juggler.DataSource
     dsConfig: object = config,
   ) {
     super(dsConfig);
-  }
-
-  /**
-   * Start the datasource when application is started
-   */
-  start(): ValueOrPromise<void> {
-    // Add your logic here to be invoked when the application is started
-  }
-
-  /**
-   * Disconnect the datasource when application is stopped. This allows the
-   * application to be shut down gracefully.
-   */
-  stop(): ValueOrPromise<void> {
-    return super.disconnect();
   }
 }
--- packages/cli/snapshots/integration/generators/openapi-client.integration.snapshots.js
+++ packages/cli/snapshots/integration/generators/openapi-client.integration.snapshots.js
@@ -1022,12 +1022,7 @@ export * from './pet-store.datasource';
 
 
 exports[`openapi-generator with --client does not generates files for server with --no-server 2`] = `
-import {
-  inject,
-  lifeCycleObserver,
-  LifeCycleObserver,
-  ValueOrPromise,
-} from '@loopback/core';
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 
 const config = {
@@ -1038,6 +1033,10 @@ const config = {
   positional: true
 };
 
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The \`stop()\` method is inherited from \`juggler.DataSource\`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
 @lifeCycleObserver('datasource')
 export class PetStoreDataSource extends juggler.DataSource
   implements LifeCycleObserver {
@@ -1049,21 +1048,6 @@ export class PetStoreDataSource extends juggler.DataSource
     dsConfig: object = config,
   ) {
     super(dsConfig);
-  }
-
-  /**
-   * Start the datasource when application is started
-   */
-  start(): ValueOrPromise<void> {
-    // Add your logic here to be invoked when the application is started
-  }
-
-  /**
-   * Disconnect the datasource when application is stopped. This allows the
-   * application to be shut down gracefully.
-   */
-  stop(): ValueOrPromise<void> {
-    return super.disconnect();
   }
 }
 
@@ -1715,12 +1699,7 @@ export * from './pet-store.datasource';
 
 
 exports[`openapi-generator with --client generates all files for both server and client 4`] = `
-import {
-  inject,
-  lifeCycleObserver,
-  LifeCycleObserver,
-  ValueOrPromise,
-} from '@loopback/core';
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 
 const config = {
@@ -1731,6 +1710,10 @@ const config = {
   positional: true
 };
 
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The \`stop()\` method is inherited from \`juggler.DataSource\`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
 @lifeCycleObserver('datasource')
 export class PetStoreDataSource extends juggler.DataSource
   implements LifeCycleObserver {
@@ -1742,21 +1725,6 @@ export class PetStoreDataSource extends juggler.DataSource
     dsConfig: object = config,
   ) {
     super(dsConfig);
-  }
-
-  /**
-   * Start the datasource when application is started
-   */
-  start(): ValueOrPromise<void> {
-    // Add your logic here to be invoked when the application is started
-  }
-
-  /**
-   * Disconnect the datasource when application is stopped. This allows the
-   * application to be shut down gracefully.
-   */
-  stop(): ValueOrPromise<void> {
-    return super.disconnect();
   }
 }
 
