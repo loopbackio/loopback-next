1b47f204c471e52a7654c4b537af7b1d69ace92b
Miroslav Bajtoš <mbajtoss@gmail.com>
2020-05-28 11:01:19 +0200

    chore: rework eslint config to use proper TS projects
    Disable `createDefaultProgram` option and ensure every linted
    file belongs to some `tsconfig`.
    
    Signed-off-by: Miroslav Bajtoš <mbajtoss@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,20 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
+const {packages} = require('./lerna.json');
+const projects = packages
+  .filter(spec => spec !== 'docs')
+  .map(spec => `${spec}/tsconfig.json`);
+
 module.exports = {
+  root: true,
   extends: ['./packages/eslint-config/eslintrc.js'],
+  parserOptions: {
+    project: [
+      'tsconfig.eslint.json',
+      ...projects,
+      'packages/tsdocs/fixtures/monorepo/packages/*/tsconfig.json',
+    ],
+    tsconfigRootDir: __dirname,
+  },
 };
