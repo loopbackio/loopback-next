33049634e2320cd30e5f9403e03db17000dd49cd
Agnes Lin <agneslin.lin@ibm.com>
2020-05-29 10:25:39 -0400

    feat(repository): add hasManyThrough factory and tests
    Co-authored-by: derdeka https://github.com/derdeka
    Co-authored-by: Jam https://github.com/codejamninja

--- packages/repository/src/relations/has-many/index.ts
+++ packages/repository/src/relations/has-many/index.ts
@@ -3,7 +3,9 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-export * from './has-many.decorator';
-export * from './has-many.repository';
 export * from './has-many-repository.factory';
+export * from './has-many-through-repository.factory';
+export * from './has-many-through.repository';
+export * from './has-many.decorator';
 export * from './has-many.inclusion-resolver';
+export * from './has-many.repository';
