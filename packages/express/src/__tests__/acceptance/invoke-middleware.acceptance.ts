729e7e5ed46463e7f6c41f060f7e41ace9d34d9b
Raymond Feng <enjoyjava@gmail.com>
2020-06-04 15:18:44 -0700

    fix(express): ensure options are honored for `invokeMiddleware` resolved from the provider
    Before this change, if we have an injected `invokeMidddleware` from the
    `InvokeMiddlewareProvider`, the `next` option is not honored.

--- packages/express/src/__tests__/acceptance/invoke-middleware.acceptance.ts
+++ packages/express/src/__tests__/acceptance/invoke-middleware.acceptance.ts
@@ -3,8 +3,15 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-import {ExpressServer, invokeExpressMiddleware} from '../../';
-import {invokeMiddleware, toMiddleware} from '../../middleware';
+import {createBindingFromClass} from '@loopback/core';
+import {
+  ExpressServer,
+  invokeExpressMiddleware,
+  invokeMiddleware,
+  InvokeMiddlewareProvider,
+  toMiddleware,
+} from '../../';
+import {InvokeMiddleware} from '../../types';
 import spyMiddlewareFactory from '../fixtures/spy.middleware';
 import {spy, TestHelper} from './test-helpers';
 
@@ -42,6 +49,34 @@ describe('Express middleware registry', () => {
         chain: 'log',
         next: () => {
           return invokeMiddleware(ctx, {chain: 'mock', next});
+        },
+      });
+    });
+    await helper.client
+      .post('/hello')
+      .send('"World"')
+      .set('content-type', 'application/json')
+      .expect(200, 'Hello, Spy')
+      .expect('x-spy-log', 'POST /hello')
+      .expect('x-spy-mock', 'POST /hello');
+  });
+
+  it('invokes middleware using the provider', async () => {
+    const logMiddleware = toMiddleware(spyMiddlewareFactory({action: 'log'}));
+    const mockMiddleware = toMiddleware(spyMiddlewareFactory({action: 'mock'}));
+
+    server.middleware(logMiddleware, {chain: 'log'});
+    server.middleware(mockMiddleware, {chain: 'mock'});
+
+    const binding = createBindingFromClass(InvokeMiddlewareProvider);
+    server.add(binding);
+    const invoke = await server.get<InvokeMiddleware>(binding.key);
+
+    server.middleware(async (ctx, next) => {
+      return invoke(ctx, {
+        chain: 'log',
+        next: () => {
+          return invoke(ctx, {chain: 'mock', next});
         },
       });
     });
