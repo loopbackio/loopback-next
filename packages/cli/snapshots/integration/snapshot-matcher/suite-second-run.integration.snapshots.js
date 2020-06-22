b1794141b5015ca4bdaa04b14fe51c47f28d098f
Miroslav Bajtoš <mbajtoss@gmail.com>
2020-06-22 16:24:37 +0200

    test(cli): add a test suite for snapshot-matcher
    Add a test suite with a shared test suite to verify snapshot-matcher's
    support for this advanced test organization. This scenario is tricky
    to get right when running tests in parallel.
    
    Signed-off-by: Miroslav Bajtoš <mbajtoss@gmail.com>

---
+++ packages/cli/snapshots/integration/snapshot-matcher/suite-second-run.integration.snapshots.js
@@ -0,0 +1,17 @@
+// IMPORTANT
+// This snapshot file is auto-generated, but designed for humans.
+// It should be checked into source control and tracked carefully.
+// Re-generate by setting UPDATE_SNAPSHOTS=1 and running tests.
+// Make sure to inspect the changes in the snapshots below.
+// Do not ignore changes!
+
+'use strict';
+
+exports[`snapshot-matcher second case matches snapshot 1`] = `
+second case
+`;
+
+
+exports[`snapshot-matcher second case shared test matches snapshot 1`] = `
+common result
+`;
