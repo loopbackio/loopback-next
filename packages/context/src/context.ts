a28d53c8951bf71af62f5238d694662905d1a8f6
Raymond Feng <enjoyjava@gmail.com>
2020-07-16 10:11:10 -0700

    fix(context): destroy debug instances to avoid memory leak
    Fixes https://github.com/strongloop/loopback-next/issues/5943

--- packages/context/src/context.ts
+++ packages/context/src/context.ts
@@ -417,6 +417,9 @@ export class Context extends EventEmitter {
     this.debug('Closing context...');
     this.subscriptionManager.close();
     this.tagIndexer.close();
+    // Destroy instance level `debug` to avoid memory leak
+    // https://github.com/strongloop/loopback-next/issues/5943
+    this._debug.destroy();
   }
 
   /**
