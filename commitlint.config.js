cc7b85f5ebcaf750221bf58ad667cc673b809588
Raymond Feng <enjoyjava@gmail.com>
2020-08-19 08:49:42 -0700

    feat: check signed-off-by for commits
    Signed-off-by: Raymond Feng <enjoyjava@gmail.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -14,5 +14,6 @@ module.exports = {
     'header-max-length': [2, 'always', 100],
     'body-leading-blank': [2, 'always'],
     'footer-leading-blank': [0, 'always'],
+    'signed-off-by': [2, 'always', 'Signed-off-by:'],
   },
 };
