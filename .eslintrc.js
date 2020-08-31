ad691ca16b3f40df8a34090c61ad770576447657
Raymond Feng <enjoyjava@gmail.com>
2020-08-31 15:44:25 -0700

    chore: turn off `mocha/handle-done-callback` rule as a workaround
    The mocha plugin incorretly reports the following signature as a
    violation of `mocha/handle-done-callback` (misinterpreting `this`
    as `done`).
    
    ```ts
    before(async function setupApplication(this: Mocha.Context) {
      this.timeout(6000);
      // ...
    });
    ```
    
    See https://github.com/lo1tuma/eslint-plugin-mocha/issues/270
    
    Signed-off-by: Raymond Feng <enjoyjava@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,8 @@ module.exports = {
          * The mocha plugin reports the following signature as a violation of
          * `mocha/handle-done-callback` (misinterpreting `this` as `done`).
          *
+         * See https://github.com/lo1tuma/eslint-plugin-mocha/issues/270
+         *
          * ```ts
          * before(async function setupApplication(this: Mocha.Context) {
          *   this.timeout(6000);
