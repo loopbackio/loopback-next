dbe382d9afc6c3539294cb23f48825091416a46c
Diana Lau <dhmlau@ca.ibm.com>
2020-09-14 10:10:03 -0400

    feat(example-passport-login): add Twitter impl
    Signed-off-by: Diana Lau <dhmlau@ca.ibm.com>

--- examples/passport-login/src/authentication-interceptors/index.ts
+++ examples/passport-login/src/authentication-interceptors/index.ts
@@ -3,8 +3,9 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-export * from './types';
-export * from './oauth2.interceptor';
-export * from './session.interceptor';
 export * from './facebook.interceptor';
 export * from './google.interceptor';
+export * from './oauth2.interceptor';
+export * from './session.interceptor';
+export * from './twitter.interceptor';
+export * from './types';
---
+++ examples/passport-login/src/authentication-strategy-providers/twitter.ts
@@ -0,0 +1,33 @@
+// Copyright IBM Corp. 2020. All Rights Reserved.
+// Node module: @loopback/example-passport-login
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import {UserIdentityService} from '@loopback/authentication';
+import {bind, BindingScope, inject, Provider} from '@loopback/core';
+import {Profile} from 'passport';
+import {IStrategyOption, Strategy as TwitterStrategy} from 'passport-twitter';
+import {verifyFunctionFactory} from '../authentication-strategies/types';
+import {User} from '../models';
+import {UserServiceBindings} from '../services';
+
+@bind.provider({scope: BindingScope.SINGLETON})
+export class TwitterOauth implements Provider<TwitterStrategy> {
+  strategy: TwitterStrategy;
+
+  constructor(
+    @inject('twitterOAuthOptions')
+    public oauthOptions: IStrategyOption,
+    @inject(UserServiceBindings.PASSPORT_USER_IDENTITY_SERVICE)
+    public userService: UserIdentityService<Profile, User>,
+  ) {
+    this.strategy = new TwitterStrategy(
+      this.oauthOptions,
+      verifyFunctionFactory(this.userService),
+    );
+  }
+
+  value() {
+    return this.strategy;
+  }
+}
