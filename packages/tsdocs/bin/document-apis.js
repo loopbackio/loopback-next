cf0b7e1237c1191a826161fefb3f88cc6e867c45
Raymond Feng <enjoyjava@gmail.com>
2021-01-08 10:30:28 -0800

    feat: make @loopback/tsdocs public so that it can be used by other projects
    Signed-off-by: Raymond Feng <enjoyjava@gmail.com>

---
+++ packages/tsdocs/bin/document-apis.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+// Copyright IBM Corp. 2021. All Rights Reserved.
+// Node module: @loopback/tsdocs
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+if (require.main === module) {
+  require('@microsoft/api-documenter/lib/start');
+}
