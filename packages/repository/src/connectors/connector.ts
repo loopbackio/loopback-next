99cbf2aa488715cb2b5ca0186dd257ac65212833
Rifa Achrinza <25147899+achrinza@users.noreply.github.com>
2021-01-16 19:32:23 +0800

    feat: add connector interface enums
    Signed-off-by: Rifa Achrinza <25147899+achrinza@users.noreply.github.com>

--- packages/repository/src/connectors/connector.ts
+++ packages/repository/src/connectors/connector.ts
@@ -13,12 +13,52 @@ import {
 import {Model} from '../model';
 
 /**
+ * Interfaces adopted by a {@link Connector}.
+ *
+ * @experimental
+ */
+export namespace ConnectorInterfaces {
+  /**
+   * Strong relation interfaces adopted by a {@link Connector}
+   *
+   * @experimental
+   */
+  export const enum StrongRelation {
+    BELONGS_TO = 'strongBelongsTo',
+    HAS_ONE = 'strongHasOne',
+    HAS_MANY = 'strongHasMany',
+    HAS_MANY_THROUGH = 'strongHasManyThrough',
+    HAS_AND_BELONGS_TO_MANY = 'strongHasAndBelongsToMany',
+    EMBEDS_ONE = 'strongEmbedsOne',
+    EMBEDS_MANY = 'strongEmbedsMany',
+    REFERNCES_MANY = 'strongReferencesMany',
+  }
+
+  /**
+   * Strong query join interfaces adopted by a {@link Connector}
+   *
+   * @experimental
+   */
+  export const enum StrongJoins {
+    INNER = 'strongInnerJoin',
+    LEFT = 'strongLeftJoin',
+    RIGHT = 'strongRightJoin',
+    FULL = 'strongFullJoin',
+    CARTESIAN = 'strongCartesianJoin',
+  }
+}
+
+/**
  * Common properties/operations for connectors
  */
 export interface Connector {
   name: string; // Name/type of the connector
   configModel?: Model; // The configuration model
-  interfaces?: string[]; // A list of interfaces implemented by the connector
+  interfaces?: (
+    | string
+    | ConnectorInterfaces.StrongRelation
+    | ConnectorInterfaces.StrongJoins
+  )[]; // A list of interfaces implemented by the connector
   connect(): Promise<void>; // Connect to the underlying system
   disconnect(): Promise<void>; // Disconnect from the underlying system
   ping(): Promise<void>; // Ping the underlying system
