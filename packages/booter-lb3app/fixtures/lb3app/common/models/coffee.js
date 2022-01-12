9595f4f77c86e5f8503afc46bee7962945b14e84
Francisco Buceta <frbuceta@gmail.com>
2022-01-12 11:40:43 +0100

    feat: remove booter-lb3app package & lb3-application example
    Signed-off-by: Francisco Buceta <frbuceta@gmail.com>

--- packages/booter-lb3app/fixtures/lb3app/common/models/coffee.js
+++
@@ -1,9 +0,0 @@
-// Copyright IBM Corp. 2019,2020. All Rights Reserved.
-// Node module: @loopback/booter-lb3app
-// This file is licensed under the MIT License.
-// License text available at https://opensource.org/licenses/MIT
-
-'use strict';
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
-module.exports = function (Coffee) {};
