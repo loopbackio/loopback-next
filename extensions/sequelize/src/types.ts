5b7e6cb8f17b45cdd9bcf8c7d81324cad38af2c7
KalleV <kvirtaneva@gmail.com>
2023-11-16 11:44:14 -0500

    fix(sequelize): extend Inclusion interface with "required" property
    This resolves errors such as the following when trying to use the
    new property with a repository call:
    ```ts
    Type '{ relation: string; scope: { where: { id: number; }; }; required: true; }'
    is not assignable to type 'InclusionFilter'.
      Object literal may only specify known properties, and 'required'
    does not exist in type 'Inclusion'.
    
    53           required: true
    ```
    
    Signed-off-by: KalleV <kvirtaneva@gmail.com>

--- extensions/sequelize/src/types.ts
+++ extensions/sequelize/src/types.ts
@@ -3,6 +3,18 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
+declare module '@loopback/repository' {
+  interface Inclusion {
+    /**
+     * Setting this option to true will result in an inner join query that
+     * explicitly requires the specified condition for the child model.
+     *
+     * @see https://loopback.io/pages/en/lb4/readmes/loopback-next/extensions/sequelize/#inner-join
+     */
+    required?: boolean;
+  }
+}
+
 /**
  * Interface defining the component's options object
  */
