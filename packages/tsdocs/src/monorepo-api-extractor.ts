a5e5b1055d099572aed73d217aa6a04e4990dfc6
dhmlau <dhmlau@ca.ibm.com>
2023-12-05 21:28:54 -0500

    fix(tsdocs): disable some repos to gen tsdocs
    Signed-off-by: dhmlau <dhmlau@ca.ibm.com>

--- packages/tsdocs/src/monorepo-api-extractor.ts
+++ packages/tsdocs/src/monorepo-api-extractor.ts
@@ -20,8 +20,8 @@ import path from 'path';
 import {
   DEFAULT_APIDOCS_EXTRACTION_PATH,
   ExtractorOptions,
-  getPackagesWithTsDocs,
   LernaPackage,
+  getPackagesWithTsDocs,
   typeScriptPath,
 } from './helper';
 const debug = debugFactory('loopback:tsdocs');
@@ -67,6 +67,17 @@ export async function runExtractorForMonorepo(options: ExtractorOptions = {}) {
   const errors: Record<string, unknown> = {};
 
   for (const pkg of packages) {
+    // TODO: api-extractor failed to generate apidocs for the repos below.
+    // Excluding them for now
+    // https://github.com/loopbackio/loopback-next/issues/10205
+    if (
+      pkg.name === '@loopback/typeorm' ||
+      pkg.name === '@loopback/boot' ||
+      pkg.name === '@loopback/express' ||
+      pkg.name === '@loopback/repository' ||
+      pkg.name === '@loopback/service-proxy'
+    )
+      continue;
     /* istanbul ignore if  */
     const err = invokeExtractorForPackage(pkg, options);
     if (err != null) {
