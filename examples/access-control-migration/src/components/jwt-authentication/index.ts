6bcfd1174832653a92106b200d7fa34c3fc696a2
Muhammad Aaqil <aaqilcs102@gmail.com>
2024-11-10 15:25:24 +0500

    fix: replace custom authentication-jwt component with @loopback/authentication-jwt
    Signed-off-by: Muhammad Aaqil <aaqilcs102@gmail.com>

--- examples/access-control-migration/src/components/jwt-authentication/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright IBM Corp. and LoopBack contributors 2020. All Rights Reserved.
-// Node module: @loopback/example-access-control-migration
-// This file is licensed under the MIT License.
-// License text available at https://opensource.org/licenses/MIT
-
-export * from './jwt-authentication-component';
-export * from './keys';
-export * from './services';
